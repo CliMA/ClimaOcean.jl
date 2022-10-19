eae538f41fc83cca3485eac262354294be1ceb56
Gregory Wagner <wagner.greg@gmail.com>
2022-10-18 18:29:08 -0600

    Change initial conditions name

--- src/ClimaOcean.jl
+++ src/ClimaOcean.jl
@@ -6,9 +6,9 @@ using DataDeps
 function __init__()
     branch_url = "https://github.com/CliMA/OceananigansArtifacts.jl/raw/glw/near-global-data"
     dir = "lat_lon_bathymetry_and_fluxes"
-    bathymetry_name = "bathymetry_lat_lon_360x150.jld2"
-    initial_conditions_name = "initial_conditions_360x150x48.jld2"
-    surface_boundary_conditions_name = "surface_boundary_conditions_360x150.jld2"
+    bathymetry_name                  = "bathymetry_lat_lon_360_150.jld2"
+    initial_conditions_name          = "initial_conditions_month_01_360_150_48.jld2"
+    surface_boundary_conditions_name = "surface_boundary_conditions_12_months_360_150.jld2"
 
     bathymetry_url = joinpath(branch_url, dir, bathymetry_name)
     initial_conditions_url = joinpath(branch_url, dir, initial_conditions_name)
