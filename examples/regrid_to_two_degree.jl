77db5e9967dbccc85e1eaef722781b053e912180
Gregory Wagner <wagner.greg@gmail.com>
2023-02-22 15:56:06 -0600

    Update

--- examples/regrid_to_two_degree.jl
+++ examples/regrid_to_two_degree.jl
@@ -10,12 +10,15 @@ one_degree_grid = LatitudeLongitudeGrid(size = (360, 150, 1),
                                         z = (0, 1),
                                         topology = (Periodic, Bounded, Bounded))
 
-bathymetry_path = datadep"near_global_one_degree/bathymetry_lat_lon_360_150.jld2"
+bathymetry_path = "near_global_bathymetry_360_150.jld2" #datadep"near_global_one_degree/bathymetry_lat_lon_360_150.jld2"
 file = jldopen(bathymetry_path)
 bathymetry_data = file["bathymetry"]
 close(file)
 
 bathymetry = Field{Center, Center, Nothing}(one_degree_grid)
+
+land = bathymetry_data .> 0
+bathymetry_data[land] .= 0
 bathymetry .= bathymetry_data
 
 one_two_degree_grid = LatitudeLongitudeGrid(size = (180, 150, 1),
@@ -39,6 +42,6 @@ regrid!(bathymetry_two_degree, bathymetry_12)
 fig = Figure()
 ax1 = Axis(fig[1, 1])
 ax2 = Axis(fig[1, 2])
-heatmap!(ax1, interior(bathymetry))
-heatmap!(ax2, interior(bathymetry_two_degree))
+heatmap!(ax1, interior(bathymetry, :, :, 1))
+heatmap!(ax2, interior(bathymetry_two_degree, :, :, 1))
 display(fig)
