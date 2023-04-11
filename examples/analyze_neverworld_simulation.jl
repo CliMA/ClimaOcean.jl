5d2822c231714a1484607301113f025cf169f8c2
Gregory Wagner <wagner.greg@gmail.com>
2023-04-10 20:24:58 -0400

    Update climatology script

--- examples/analyze_neverworld_simulation.jl
+++ examples/analyze_neverworld_simulation.jl
@@ -1,42 +1,42 @@
 using Oceananigans
-using Oceananigans.ImmersedBoundaries: mask_immersed_field!
 using Oceananigans.Units
 using Printf
 using JLD2
 
+dir = "archive"
 backend = OnDisk()
-bt = FieldTimeSeries("neverworld_xyz.jld2", "b"; backend)
-ut = FieldTimeSeries("neverworld_xyz.jld2", "u"; backend)
-ζt = FieldTimeSeries("neverworld_xyz.jld2", "ζ"; backend)
-et = FieldTimeSeries("neverworld_xyz.jld2", "e"; backend)
-κt = FieldTimeSeries("neverworld_xyz.jld2", "κᶜ"; backend)
-
+bt = FieldTimeSeries("$dir/neverworld_xyz.jld2", "b"; backend)
 t = bt.times
 Nt = length(t)
 grid = bt.grid
 
-averaging_years = 5
+Nyears = 20
+t_end = 200 * 360 * day # 200 "years"
+t_start = (200 - Nyears) * 360 * day # 200 "years"
 
-for month in 1:12
-    n_start = Nt - 12 * averaging_years - month + 1
-    n_final = Nt - month + 1
-    n_average = n_start:12:n_final
+t = bt.times
+n_stop  = searchsortedfirst(t, t_stop)
+n_start = searchsortedfirst(t, t_start) - 1
 
-    for name in ("b", "u", "ζ", "e", "κᶜ")
-        ψt = FieldTimeSeries("neverworld_xyz.jld2", name; backend)
+for season = 1:4
+    n_average_start = n_start + season - 1
+    n_average = n_average_start:4:n_stop
+
+    for name in ("b", "u", "v", "w", "e", "κᶜ")
+        ψt = FieldTimeSeries("$dir/neverworld_xyz.jld2", name; backend)
         LX, LY, LZ = location(ψt)
         ψ_avg = Field{LX, LY, LZ}(grid)
 
         for n in n_average
-            @info name month n
+            @info name season n
             ψn = ψt[n]
             ψ_avg .+= ψn / length(n_average)
         end
         
-        filename = string("climatology_", name, "_month_", month, ".jld2")
+        filename = string("climatology_", name, "_season_", season, ".jld2")
         file = jldopen(filename, "a+")
         file[name] = ψ_avg
-        file["month"] = month
+        file["season"] = season
         close(file)
     end
 end
