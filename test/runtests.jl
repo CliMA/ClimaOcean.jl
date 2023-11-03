4e67f140793698153e0413994b2a778b7f72bbf8
Gregory Wagner <wagner.greg@gmail.com>
2023-11-02 19:44:08 -0600

    Fix bug in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ test_group = get(ENV, "TEST_GROUP", :all)
 test_group = Symbol(test_group)
 
 # Tests JRA55 utilities, plus some DataWrangling utilities
-if test_group == :jra55 || test_group = :all
+if test_group == :jra55 || test_group == :all
     include("test_jra55.jl")
 end
 
 # Tests that we can download JRA55 utilities
-if test_group == :downloading || test_group = :all
+if test_group == :downloading || test_group == :all
     include("test_downloading.jl")
 end
 
