41c68fc0b1d3d4cf79aeb69391d3324395ccea46
Gregory Wagner <wagner.greg@gmail.com>
2024-01-28 20:26:12 -0700

    Fix bug in PrescribedAtmospheres

--- src/OceanSeaIceModels/PrescribedAtmospheres.jl
+++ src/OceanSeaIceModels/PrescribedAtmospheres.jl
@@ -190,7 +190,7 @@ end
 const PTP = PhaseTransitionParameters
 @inline LH_v0(p::PTP)        = p.reference_vaporization_enthalpy
 @inline LH_s0(p::PTP)        = p.reference_sublimation_enthalpy
-@inline LH_f0(p::PTP)        = LH_s0(p) - p.LH_v0
+@inline LH_f0(p::PTP)        = LH_s0(p) - LH_v0(p)
 @inline T_freeze(p::PTP)     = p.water_freezing_temperature
 @inline T_triple(p::PTP)     = p.triple_point_temperature
 @inline T_icenuc(p::PTP)     = p.total_ice_nucleation_temperature
