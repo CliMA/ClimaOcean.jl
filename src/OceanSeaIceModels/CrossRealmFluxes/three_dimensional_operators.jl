3731a072c22c0147173ee94e40d674009e845ef9
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-03-06 15:59:47 -0500

    bugfix

--- src/OceanSeaIceModels/CrossRealmFluxes/three_dimensional_operators.jl
+++ src/OceanSeaIceModels/CrossRealmFluxes/three_dimensional_operators.jl
@@ -5,8 +5,8 @@ using Oceananigans.ImmersedBoundaries: c, f
 @inline ı(i, j, k, grid, ϕ)                    = ϕ[i, j, k]
 
 # Defining Interpolation operators for the immersed boundaries
-@inline conditional_ℑx_f(LY, LZ, i, j, k, grid, c) = ifelse(inactive_node(i, j, k, grid, c, LY, LZ), ı(i-1, j, k, grid, args...), ifelse(inactive_node(i-1, j, k, grid, c, LY, LZ), ı(i, j, k, grid, args...), ℑxᶠᵃᵃ(i, j, k, grid, args...)))
-@inline conditional_ℑy_f(LX, LZ, i, j, k, grid, c) = ifelse(inactive_node(i, j, k, grid, LX, c, LZ), ı(i, j-1, k, grid, args...), ifelse(inactive_node(i, j-1, k, grid, LX, c, LZ), ı(i, j, k, grid, args...), ℑyᵃᶠᵃ(i, j, k, grid, args...)))
+@inline conditional_ℑx_f(LY, LZ, i, j, k, grid, t) = ifelse(inactive_node(i, j, k, grid, c, LY, LZ), ı(i-1, j, k, grid, t), ifelse(inactive_node(i-1, j, k, grid, c, LY, LZ), ı(i, j, k, grid, t), ℑxᶠᵃᵃ(i, j, k, grid, t)))
+@inline conditional_ℑy_f(LX, LZ, i, j, k, grid, t) = ifelse(inactive_node(i, j, k, grid, LX, c, LZ), ı(i, j-1, k, grid, t), ifelse(inactive_node(i, j-1, k, grid, LX, c, LZ), ı(i, j, k, grid, t), ℑyᵃᶠᵃ(i, j, k, grid, t)))
 
-ℑxᶠᶜᶜ(i, j, k, grid, c) = conditional_ℑx_f(c, c, i, j, k, grid, c)
-ℑyᶜᶠᶜ(i, j, k, grid, c) = conditional_ℑy_f(c, c, i, j, k, grid, c)
+ℑxᶠᶜᶜ(i, j, k, grid, t) = conditional_ℑx_f(c, c, i, j, k, grid, t)
+ℑyᶜᶠᶜ(i, j, k, grid, t) = conditional_ℑy_f(c, c, i, j, k, grid, t)
