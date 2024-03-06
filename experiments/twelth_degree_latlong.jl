8ef9d74d3ca0afa71dc29170fefd3ce8fd6827ba
Andre Nogueira Souza <sandre@satori-login-001.mit.edu>
2024-03-05 19:14:04 -0500

    working

--- experiments/twelth_degree_latlong.jl
+++ experiments/twelth_degree_latlong.jl
@@ -1,3 +1,6 @@
+using MPI
+MPI.Init()
+
 using Oceananigans
 using Oceananigans: architecture
 using ClimaOcean
@@ -18,27 +21,39 @@ using JLD2
 ##### Global Ocean at 1/12th of a degree
 #####
 
-bathymetry_file = nothing # "bathymetry_quarter.jld2"
+bathymetry_file = "bathymetry12.jld2"
 
 # 100 vertical levels
-z_faces = exponential_z_faces(Nz=50, depth=6000)
+z_faces = exponential_z_faces(Nz=100, depth=6000)
 
-Nx = 360
-Ny = 150
+Nx = 4320
+Ny = 1800
 Nz = length(z_faces) - 1
 
-arch = Distributed(CPU(), partition = Partition(4))
+arch = Distributed(GPU(), partition = Partition(8))
 
-grid = load_balanced_regional_grid(arch; 
-                                   size = (Nx, Ny, Nz), 
-                                   z = z_faces, 
-                                   latitude  = (-75, 75),
-                                   longitude = (0, 360),
-                                   halo = (7, 7, 7),
-                                   interpolation_passes = 20,
-                                   minimum_depth = 10,
-                                   connected_regions_allowed = 3, # We allow the oceans, the med, the bering sea
-                                   bathymetry_file)
+grid = LatitudeLongitudeGrid(arch; 
+			     size = (Nx, Ny, Nz),
+                             z = z_faces,
+                             latitude  = (-75, 75),
+                             longitude = (0, 360),
+                             halo = (7, 7, 7))
+
+bathymetry = jldopen(bathymetry_file)["bathymetry"]
+
+@show size(bathymetry)
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bathymetry); active_cells_map = true)
+
+# grid = load_balanced_regional_grid(arch; 
+#                                    size = (Nx, Ny, Nz), 
+#                                    z = z_faces, 
+#                                    latitude  = (-75, 75),
+#                                    longitude = (0, 360),
+#                                    halo = (7, 7, 7),
+#                                    interpolation_passes = 20,
+#                                    minimum_depth = 10,
+#                                    connected_regions_allowed = 3, # We allow the oceans, the med, the bering sea
+#                                    bathymetry_file)
  
 @show grid                                   
                               
@@ -46,7 +61,7 @@ grid = load_balanced_regional_grid(arch;
 ##### The Ocean component
 #####                             
 
-free_surface = SplitExplicitFreeSurface(; grid, cfl=0.7, fixed_Δt=10minutes)
+free_surface = SplitExplicitFreeSurface(; grid, cfl=0.7, fixed_Δt=270)
 
 ocean = ocean_simulation(grid; free_surface)
 model = ocean.model
@@ -61,7 +76,7 @@ set!(model,
 ##### The atmosphere
 #####
 
-backend    = JRA55NetCDFBackend(10) 
+backend    = JRA55NetCDFBackend(5) 
 atmosphere = JRA55_prescribed_atmosphere(arch; backend)
 radiation  = Radiation()
 
@@ -87,18 +102,11 @@ end
 
 ocean.callbacks[:progress] = Callback(progress, IterationInterval(1))
 
-ocean.output_writers[:snapshots] = JLD2OutputWriter(model, merge(model.tracers, model.velocities),
-                                                    schedule = TimeInterval(30days),
-                                                    overwrite_existing = true,
-                                                    array_type = Array{Float32},
-                                                    filename = "snapshots_$(arch.local_rank)")
-
 ocean.output_writers[:surface] = JLD2OutputWriter(model, merge(model.tracers, model.velocities),
-                                                  schedule = TimeInterval(1days),
+                                                  schedule = TimeInterval(30days),
                                                   overwrite_existing = true,
-                                                  indices = (:, :, grid.Nz),
                                                   array_type = Array{Float32},
-                                                  filename = "surface_$(arch.local_rank)")
+                                                  filename = "snapshots_$(arch.local_rank)")
 
 ocean.output_writers[:checkpoint] = Checkpointer(model, 
                                                  schedule = TimeInterval(60days),
