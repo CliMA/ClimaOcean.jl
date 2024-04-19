e49973b6c62b39e5104cf7c419d2300e37c4694b
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-04-19 15:14:49 -0400

    let's go

--- quarter_degree_omip/quarter_degree_omip.jl
+++ quarter_degree_omip/quarter_degree_omip.jl
@@ -16,16 +16,17 @@ using ClimaOcean.OceanSeaIceModels.CrossRealmFluxes: Radiation
 using ClimaOcean.VerticalGrids: exponential_z_faces
 using ClimaOcean.JRA55
 using ClimaOcean.JRA55: JRA55NetCDFBackend, JRA55_prescribed_atmosphere
+using ClimaOcean.Bathymetry
 
 include("correct_oceananigans.jl")
 
 #####
-##### Global Ocean at 1/12th of a degree
+##### Global Ocean at 1/4th of a degree
 #####
 
 bathymetry_file = nothing # "bathymetry_tmp.jld2"
 
-# 100 vertical levels
+# 60 vertical levels
 z_faces = exponential_z_faces(Nz=60, depth=6500)
 
 Nx = 720
@@ -34,11 +35,29 @@ Nz = length(z_faces) - 1
 
 arch = CPU() #Distributed(GPU(), partition = Partition(2))
 
-underlying_grid = TripolarGrid(arch; size = (Nx, Ny, Nz), halo = (7, 7, 7))
+grid = TripolarGrid(arch; size = (Nx, Ny, Nz), halo = (7, 7, 7), z = z_faces)
 
 bottom_height = retrieve_bathymetry(grid, bathymetry_file; 
                                     minimum_depth = 10,
                                     interpolation_passes = 20,
-                                    connected_regions_allowed = 3)
+                                    connected_regions_allowed = 1)
  
-grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom_height); active_cells_map = true) 
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map = true) 
+
+#####
+##### The Ocean component
+#####                             
+
+const Lz = grid.Lz
+const  h = Nz / 4.5
+
+@inline exponential_profile(z; Lz, h) = (exp(z / h) - exp( - Lz / h)) / (1 - exp( - Lz / h))
+@inline νz(x, y, z, t) = 1e-4 + (5e-3 - 1e-4) * exponential_profile(z; Lz, h)
+
+free_surface = SplitExplicitFreeSurface(; grid, cfl=0.7, fixed_Δt = 20minutes)
+vertical_diffusivity = VerticalScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ = 5e-5, ν = 5e-3)
+
+closure = (RiBasedVerticalDiffusivity(), vertical_diffusivity)
+
+ocean = ocean_simulation(grid; free_surface, closure) 
+model = ocean.model
