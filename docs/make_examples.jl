027a74b23e695c2962a73e1c7855a92546d11db6
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-10 20:21:31 -0400

    add single column to docs

--- docs/make_examples.jl
+++ docs/make_examples.jl
@@ -16,6 +16,7 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
 to_be_literated = [
     "inspect_ecco2_data.jl",
+    "JRA55forced_single_column_simulation.jl",
     "near_global_omip_simulation.jl"
 ]
 
