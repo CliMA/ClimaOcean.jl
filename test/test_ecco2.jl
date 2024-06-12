470c65ecc6623a29553e87e4465be233ce9a551f
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-12 14:43:49 -0400

    another bugfix

--- test/test_ecco2.jl
+++ test/test_ecco2.jl
@@ -43,7 +43,7 @@ end
 
 @testset "setting a field with ECCO2" begin
     for arch in test_architectures
-        grid  = LatitudeLongitudeGrid(size = (10, 10, 10), latitude = (0, 80), longitude = (-5, 5), z = (-200, 0))
+        grid  = LatitudeLongitudeGrid(size = (10, 10, 10), latitude = (-60, -40), longitude = (175, 185), z = (-200, 0))
         field = CenterField(grid)
         set!(field, ECCO2Metadata(:temperature)) 
         set!(field, ECCO2Metadata(:salinity))
