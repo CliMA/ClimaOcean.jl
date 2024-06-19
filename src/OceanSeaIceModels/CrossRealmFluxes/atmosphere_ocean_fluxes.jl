e1f386d9006728d17dc3d728478b7ec1f6838e41
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-19 16:39:48 -0400

    correct the functions

--- src/OceanSeaIceModels/CrossRealmFluxes/atmosphere_ocean_fluxes.jl
+++ src/OceanSeaIceModels/CrossRealmFluxes/atmosphere_ocean_fluxes.jl
@@ -147,7 +147,7 @@ limit_fluxes_over_sea_ice!(args...) = nothing
     # Convert the native grid velocities to a zonal - meridional 
     # frame of reference (assuming the frame of reference is 
     # latitude - longitude here, we might want to change it)
-    uₒ, vₒ = convert_to_intrinsic_reference_frame(i, j, kᴺ, grid, uₒ, vₒ)
+    uₒ, vₒ = convert_to_extrinsic_reference_frame(i, j, kᴺ, grid, uₒ, vₒ)
         
     @inbounds begin
         # Atmos state, which is _assumed_ to exist at location = (c, c, nothing)
@@ -209,7 +209,7 @@ limit_fluxes_over_sea_ice!(args...) = nothing
 
     # Convert back from a zonal - meridional flux to the frame of 
     # reference of the native ocean grid
-    τˣ, τʸ = convert_to_extrinsic_reference_frame(i, j, kᴺ, grid, turbulent_fluxes.x_momentum, 
+    τˣ, τʸ = convert_to_intrinsic_reference_frame(i, j, kᴺ, grid, turbulent_fluxes.x_momentum, 
                                                                   turbulent_fluxes.y_momentum)
 
     @inbounds begin
