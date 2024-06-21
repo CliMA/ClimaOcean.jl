9b947bbece78d7226a1a6440606190fbc697391b
Simone Silvestri <silvestri.simone0@gmail.com>
2024-06-21 13:57:50 -0400

    adding docs (#93)

--- docs/make.jl
+++ docs/make.jl
@@ -45,6 +45,12 @@ pages = [
         "Private"        => "library/internals.md",
         "Function index" => "library/function_index.md",
         ],
+
+    "Examples" => [
+        "Inspect ECCO2 data" => "literated/inspect_ecco2_data.md",
+        "Surface fluxes" => "literated/generate_surface_fluxes.md",
+        "Single column simulation" => "literated/single_column_simulation.md",
+        ]
 ]
 
 makedocs(
