2a6d7a919a811eef37166933f9d3bcd5986d3e4f
simone-silvestri <silvestri.simone0@gmail.com>
2024-08-02 15:34:01 -0400

    progress it stuck on single_column_grid

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 to_be_literated = [
     "inspect_ecco_data.jl",
     "generate_surface_fluxes.jl",
-    "single_column_simulation.jl",
+    # "single_column_simulation.jl",
     # "mediterranean_simulation_with_ecco_restoring.jl",
     "near_global_omip_simulation.jl"
 ]
@@ -53,7 +53,7 @@ pages = [
     "Examples" => [
         "Inspect ECCO2 data" => "literated/inspect_ecco_data.md",
         "Surface fluxes" => "literated/generate_surface_fluxes.md",
-        "Single column simulation" => "literated/single_column_simulation.md",
+        # "Single column simulation" => "literated/single_column_simulation.md",
         # "Mediterranean simulation with ECCO restoring" => "literated/mediterranean_simulation_with_ecco_restoring.md",
         "Near global OMIP simulation" => "literated/near_global_omip_simulation.md",
         ]
