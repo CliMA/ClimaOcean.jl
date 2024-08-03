7f2936f9aee11fefcffc098f070c4600971cd6b3
simone-silvestri <silvestri.simone0@gmail.com>
2024-08-03 13:16:42 -0400

    another bugfix

--- examples/near_global_omip_simulation.jl
+++ examples/near_global_omip_simulation.jl
@@ -158,13 +158,13 @@ fluxes = (u = model.velocities.u.boundary_conditions.top.condition,
 output_kwargs = (; overwrite_existing = true, array_type = Array{Float32})
 
 ocean.output_writers[:fluxes] = JLD2OutputWriter(model, fluxes;
-                                                 schedule = TimeInterval(1day),
+                                                 schedule = TimeInterval(1days),
                                                  overwrite_existing = true,
                                                  filename = "surface_fluxes",
                                                  output_kwargs...)
 
 ocean.output_writers[:surface] = JLD2OutputWriter(model, merge(model.tracers, model.velocities);
-                                                  schedule = TimeInterval(1day),
+                                                  schedule = TimeInterval(1days),
                                                   filename = "surface",
                                                   indices = (:, :, grid.Nz),
                                                   output_kwargs...)
