edecaf9eef8aa87c35560a561c6839bef23ac43b
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-05 17:59:27 -0400

    Fix typo (#114)
    * fix typo
    
    * some more bugfixes

--- src/OceanSeaIceModels/CrossRealmFluxes/similarity_theory_turbulent_fluxes.jl
+++ src/OceanSeaIceModels/CrossRealmFluxes/similarity_theory_turbulent_fluxes.jl
@@ -197,7 +197,7 @@ end
 #####
 
 """
-    LogarthmicSimilarityProfile()
+    LogarithmicSimilarityProfile()
 
 Represents the classic Monin-Obukhov similarity profile, which finds that 
 
@@ -215,13 +215,13 @@ and ``Π`` is the "similarity profile",
 which is a logarithmic profile adjusted by the stability function ``ψ`` and dependent on
 the Monin-Obukhov length ``L`` and the roughness length ``ℓ``.
 """
-struct LogarthmicSimilarityProfile end
-struct COARELogarthmicSimilarityProfile end
+struct LogarithmicSimilarityProfile end
+struct COARELogarithmicSimilarityProfile end
 
-@inline similarity_profile(::LogarthmicSimilarityProfile, ψ, h, ℓ, L) =
+@inline similarity_profile(::LogarithmicSimilarityProfile, ψ, h, ℓ, L) =
     log(h / ℓ) - ψ(h / L) + ψ(ℓ / L)
 
-@inline similarity_profile(::COARELogarthmicSimilarityProfile, ψ, h, ℓ, L) =
+@inline similarity_profile(::COARELogarithmicSimilarityProfile, ψ, h, ℓ, L) =
     log(h / ℓ) - ψ(h / L)
 
 #####
