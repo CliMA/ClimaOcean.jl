1fe2ca1bbf6d8804f9d69751111ea1f5c2a6f9ee
Simone Silvestri <silvestri.simone0@gmail.com>
2024-08-08 23:40:24 -0400

    another bugfix

--- src/OceanSeaIceModels/CrossRealmFluxes/compute_cross_realm_fluxes.jl
+++ src/OceanSeaIceModels/CrossRealmFluxes/compute_cross_realm_fluxes.jl
@@ -123,8 +123,6 @@ function compute_atmosphere_ocean_fluxes!(coupled_model)
             radiation_properties,
             coupled_model.fluxes.ocean_reference_density,
             coupled_model.fluxes.ocean_heat_capacity,
-            coupled_model.fluxes.ice_density,
-            coupled_model.fluxes.ice_heat_capacity,
             coupled_model.fluxes.freshwater_density)
 
     launch!(arch, grid, :xy, reconstruct_momentum_fluxes!,
