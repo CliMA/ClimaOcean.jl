d4494695393b8ad8fa9d73889578c7303e367405
Simone Silvestri <silvestri.simone0@gmail.com>
2024-09-19 13:20:31 -0400

    test it out on 8 gpus

--- prototype_omip_simulation/omip_highres.sh
+++ prototype_omip_simulation/omip_highres.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=2
-#SBATCH --gres=gpu:2
-#SBATCH -p sched_mit_raffaele_gpu
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH -p pi_raffaele
 #SBATCH --time=120:00:00
 #SBATCH --mem=200GB
 
@@ -18,6 +18,4 @@ export OMPI_MCA_pml=^ucx
 export OMPI_MCA_osc=^ucx
 export OMPI_MCA_btl_openib_allow_ib=true
 
-julia --project --check-bounds=no -e 'using Pkg; Pkg.update("Oceananigans")'
-
-/nobackup1/users/ssilvest/openmpi/bin/mpiexec -np 2 julia --project --check-bounds=no prototype_omip_simulation.jl
+/nobackup1/users/ssilvest/openmpi/bin/mpiexec -np 4 julia --project --check-bounds=no prototype_omip_simulation.jl
