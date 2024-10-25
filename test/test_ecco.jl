6867deeeb398f398aedfaef5e72dc3c939d9fe1d
Gregory Wagner <wagner.greg@gmail.com>
2024-10-24 22:13:29 -0600

    Add a test setting to a model

--- test/test_ecco.jl
+++ test/test_ecco.jl
@@ -41,11 +41,21 @@ using Dates
     end
 end
 
-@testset "setting a field with ECCO" begin
+@testset "Setting a field with ECCO" begin
     for arch in test_architectures
-        grid  = LatitudeLongitudeGrid(size = (10, 10, 10), latitude = (-60, -40), longitude = (10, 15), z = (-200, 0))
+        grid = LatitudeLongitudeGrid(size=(10, 10, 10), latitude=(-60, -40), longitude=(10, 15), z=(-200, 0))
         field = CenterField(grid)
         set!(field, ECCOMetadata(:temperature)) 
         set!(field, ECCOMetadata(:salinity))
-    end 
+    end
 end
+
+@testset "Setting temperature and salinity to ECCO" begin
+    for arch in test_architectures
+        grid = LatitudeLongitudeGrid(size=(10, 10, 10), latitude=(-60, -40), longitude=(10, 15), z=(-200, 0))
+        ocean = ocean_simulation(grid)
+        date = DateTimeProlepticGregorian(1993, 1, 1)
+        set!(ocean.model, T=ECCOMetadata(:temperature, date), S=ECCOMetadata(:salinity, date))
+    end
+end
+
