b7dc6c1e76f60c69e59e9860f28f1608ba3eac84
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-31 12:41:36 +0100

    introducing @onrank

--- src/DataWrangling/distributed_datawrangling.jl
+++ src/DataWrangling/distributed_datawrangling.jl
@@ -16,7 +16,23 @@ Perform `exp` only on rank 0, otherwise know as "root" rank.
 Other ranks will wait for the root rank to finish before continuing
 """
 macro root(exp)
-    if MPI.Initialized() && MPI.Comm_rank(MPI.COMM_WORLD) != 0
+    command = quote
+        if MPI.Initialized() 
+            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+                $exp
+            end
+            global_barrier()
+        end
+    end
+    return esc(command)
+end
+
+"""
+Perform `exp` only on rank `$rank`
+Other ranks will wait for the root rank to finish before continuing
+"""
+macro onrank(rank, exp)
+    if MPI.Initialized() && MPI.Comm_rank(MPI.COMM_WORLD) != eval(rank)
         command = quote
           global_barrier()
         end
@@ -36,24 +52,24 @@ macro distribute(exp)
         error("The `@distribute` macro expects a `for` loop")
     end
 
-    mpi_initialized = MPI.Initialized()
-    if !mpi_initialized
-        return esc(exp)
-    end
-
-    rank     = MPI.Comm_rank(MPI.COMM_WORLD)
-    nprocs   = MPI.Comm_size(MPI.COMM_WORLD)
     iterable = exp.args[1].args[2]
     variable = exp.args[1].args[1]
     forbody  = exp.args[2]
 
     new_loop = quote
-        for (counter, $variable) in enumerate($iterable)
-            if $variable % $nprocs == $rank
-                $forbody
+        mpi_initialized = MPI.Initialized()
+        if !mpi_initialized
+            $exp
+        else
+            rank   = MPI.Comm_rank(MPI.COMM_WORLD)
+            nprocs = MPI.Comm_size(MPI.COMM_WORLD)
+            for (counter, $variable) in enumerate($iterable)
+                if $variable % nprocs == rank
+                    $forbody
+                end
             end
+            global_barrier()
         end
-        global_barrier()
     end
 
     return esc(new_loop)
