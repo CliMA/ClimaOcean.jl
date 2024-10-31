4741ec1c37c151123d180c9d0e07b08285941d9c
Simone Silvestri <silvestri.simone0@gmail.com>
2024-10-31 11:10:16 +0100

    back also here

--- src/OceanSeaIceModels/OceanSeaIceModels.jl
+++ src/OceanSeaIceModels/OceanSeaIceModels.jl
@@ -22,10 +22,6 @@ using ClimaOcean: stateindex
 using KernelAbstractions: @kernel, @index
 using KernelAbstractions.Extras.LoopInfo: @unroll
 
-function surface_velocities end
-function surface_tracers end
-function surface_horizontal_velocities end
-function surface_active_tracers end
 function downwelling_radiation end
 function freshwater_flux end
 function reference_density end
@@ -63,5 +59,43 @@ import .CrossRealmFluxes:
 const NoAtmosphereModel = OceanSeaIceModel{<:Any, Nothing}
 
 compute_atmosphere_ocean_fluxes!(coupled_model::NoAtmosphereModel) = nothing
+
+#####
+##### A fairly dumb, but nevertheless effective "sea ice model"
+#####
+
+struct FreezingLimitedOceanTemperature{L}
+    liquidus :: L
+end
+
+const FreezingLimitedCoupledModel = OceanSeaIceModel{<:FreezingLimitedOceanTemperature}
+
+sea_ice_concentration(::FreezingLimitedOceanTemperature) = nothing
+
+function compute_sea_ice_ocean_fluxes!(cm::FreezingLimitedCoupledModel)
+    ocean = cm.ocean
+    liquidus = cm.sea_ice.liquidus
+    grid = ocean.model.grid
+    arch = architecture(grid)
+    Sₒ = ocean.model.tracers.S
+    Tₒ = ocean.model.tracers.T
+
+    launch!(arch, grid, :xyz,  above_freezing_ocean_temperature!, Tₒ, Sₒ, liquidus)
+
+    return nothing
+end
+
+@kernel function above_freezing_ocean_temperature!(Tₒ, Sₒ, liquidus)
+
+    i, j, k = @index(Global, NTuple)
+
+    @inbounds begin
+        Sᵢ = Sₒ[i, j, k]
+        Tᵢ = Tₒ[i, j, k]
+    end
+
+    Tₘ = melting_temperature(liquidus, Sᵢ)
+    @inbounds Tₒ[i, j, k] = ifelse(Tᵢ < Tₘ, Tₘ, Tᵢ)
+end
 
 end # module
