21d944f775a5e0c78107da83472311589762cc8d
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-01 15:11:29 -0400

    remove surface fluxes

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 to_be_literated = [
     "inspect_ecco_data.jl",
     "generate_bathymetry.jl",
-    "generate_surface_fluxes.jl",
+    # "generate_surface_fluxes.jl",
     "single_column_os_papa_simulation.jl",
     # "mediterranean_simulation_with_ecco_restoring.jl",
     "near_global_ocean_simulation.jl"
@@ -50,7 +50,7 @@ pages = [
     "Examples" => [
         "Inspect ECCO2 data" => "literated/inspect_ecco_data.md",
         "Generate bathymetry" => "literated/generate_bathymetry.md",
-        "Surface fluxes" => "literated/generate_surface_fluxes.md",
+        # "Surface fluxes" => "literated/generate_surface_fluxes.md",
         "Single column simulation" => "literated/single_column_os_papa_simulation.md",
         # "Mediterranean simulation with ECCO restoring" => "literated/mediterranean_simulation_with_ecco_restoring.md",
         "Near-global Ocean simulation" => "literated/near_global_ocean_simulation.md",
