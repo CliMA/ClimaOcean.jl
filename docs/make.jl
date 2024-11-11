465b62c0e2ade4a71964919f6ad270811cddc2f9
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-11 18:47:08 +1100

    add the near-global example

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ to_be_literated = [
     # "generate_surface_fluxes.jl",
     # "single_column_os_papa_simulation.jl",
     # "mediterranean_simulation_with_ecco_restoring.jl",
-    # "near_global_ocean_simulation.jl"
+    "near_global_ocean_simulation.jl"
 ]
 
 for file in to_be_literated
@@ -46,7 +46,7 @@ pages = [
         # "Surface fluxes" => "literated/generate_surface_fluxes.md",
         # "Single column simulation" => "literated/single_column_os_papa_simulation.md",
         # "Mediterranean simulation with ECCO restoring" => "literated/mediterranean_simulation_with_ecco_restoring.md",
-        # "Near-global Ocean simulation" => "literated/near_global_ocean_simulation.md",
+        "Near-global Ocean simulation" => "literated/near_global_ocean_simulation.md",
         ],
 
     "Library" => [ 
