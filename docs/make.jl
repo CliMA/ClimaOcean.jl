cb52e94e414d3cbce9621b4960798b361b3b4b29
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-11 19:12:51 +1100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,12 +13,12 @@ const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
 to_be_literated = [
-    # "inspect_ecco_data.jl",
+    "inspect_ecco_data.jl",
     "generate_bathymetry.jl",
     # "generate_surface_fluxes.jl",
-    # "single_column_os_papa_simulation.jl",
+    "single_column_os_papa_simulation.jl",
     # "mediterranean_simulation_with_ecco_restoring.jl",
-    # "near_global_ocean_simulation.jl"
+    near_global_ocean_simulation.jl"
 ]
 
 for file in to_be_literated
@@ -41,12 +41,12 @@ pages = [
     "Home" => "index.md",
 
     "Examples" => [
-        # "Inspect ECCO2 data" => "literated/inspect_ecco_data.md",
+        "Inspect ECCO2 data" => "literated/inspect_ecco_data.md",
         "Generate bathymetry" => "literated/generate_bathymetry.md",
         # "Surface fluxes" => "literated/generate_surface_fluxes.md",
-        # "Single column simulation" => "literated/single_column_os_papa_simulation.md",
+        "Single column simulation" => "literated/single_column_os_papa_simulation.md",
         # "Mediterranean simulation with ECCO restoring" => "literated/mediterranean_simulation_with_ecco_restoring.md",
-        # "Near-global Ocean simulation" => "literated/near_global_ocean_simulation.md",
+        "Near-global Ocean simulation" => "literated/near_global_ocean_simulation.md",
         ],
 
     "Library" => [ 
