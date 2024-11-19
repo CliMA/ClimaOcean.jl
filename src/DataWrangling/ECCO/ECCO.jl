3e2d512ae07ef9d3c56cbd289d697e26b2d48760
simone-silvestri <silvestri.simone0@gmail.com>
2024-11-18 20:27:59 -0500

    correct stuff

--- src/DataWrangling/ECCO/ECCO.jl
+++ src/DataWrangling/ECCO/ECCO.jl
@@ -138,7 +138,7 @@ within the specified `mask`. `mask` is set to `ECCO_mask` for non-nothing
 """
 function ECCO_field(metadata::ECCOMetadata;
                     architecture = CPU(),
-                    inpainting = nothing,
+                    inpainting = NearestNeighborInterpolation(Inf),
                     mask = nothing,
                     horizontal_halo = (7, 7),
                     cache_inpainted_data = false)
