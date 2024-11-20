b24bda6f6a4a1e1b3c3e367d51b6f87aacc4ac19
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-20 18:20:46 +1100

    fix N2 limits

--- examples/single_column_os_papa_simulation.jl
+++ examples/single_column_os_papa_simulation.jl
@@ -312,7 +312,7 @@ xlims!(axTz, Tmin - 0.1, Tmax + 0.1)
 
 Nmax = maximum(interior(N²))
 Nmin = minimum(interior(N²))
-xlims!(axNz, Nmin / 2, Nmin * 1.1)
+xlims!(axNz, Nmin/2, Nmax * 1.1)
 
 κmax = maximum(interior(κ))
 xlims!(axκz, 1e-9, κmax * 1.1)
