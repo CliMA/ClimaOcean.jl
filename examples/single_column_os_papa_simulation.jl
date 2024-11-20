ed0af9aa20113e50ccb47328694ceccc989c2162
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-20 18:43:38 +1100

    3hrs is plenty

--- examples/single_column_os_papa_simulation.jl
+++ examples/single_column_os_papa_simulation.jl
@@ -165,7 +165,7 @@ outputs = merge(fields, fluxes)
 filename = "single_column_omip_$(location_name)"
 
 simulation.output_writers[:jld2] = JLD2OutputWriter(ocean.model, outputs; filename,
-                                                    schedule = TimeInterval(2hours),
+                                                    schedule = TimeInterval(3hours),
                                                     overwrite_existing = true)
 
 run!(simulation)
