34195b3b737ef2be43e573cc366d80c7ab3c77c3
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-20 18:30:01 -0800

    run the tests

--- src/OceanSeaIceModels/time_step_ocean_sea_ice_model.jl
+++ src/OceanSeaIceModels/time_step_ocean_sea_ice_model.jl
@@ -30,7 +30,6 @@ function time_step!(coupled_model::OceanSeaIceModel, Δt; callbacks=[], compute_
 
     # TODO after ice time-step:
     #   - Adjust ocean heat flux if the ice completely melts?
-
     ocean.Δt = Δt
     time_step!(ocean)
 
