1f0e94758a847bb39be0994193061c02734ca8f7
Simone Silvestri <silvestri.simone0@gmail.com>
2024-12-02 18:20:14 +0100

    add commuincator

--- test/test_distributed_utils.jl
+++ test/test_distributed_utils.jl
@@ -6,7 +6,7 @@ MPI.Init()
 @testset begin
     rank = MPI.Comm_rank(MPI.COMM_WORLD)
 
-    @onrank 0, begin
+    @onrank 0 begin
         @test rank == 0
     end
 
@@ -14,15 +14,15 @@ MPI.Init()
         @test rank == 0
     end
 
-    @onrank 1, begin
+    @onrank 1 begin
         @test rank == 1
     end
 
-    @onrank 2, begin
+    @onrank 2 begin
         @test rank == 2
     end
 
-    @onrank 3, begin
+    @onrank 3 begin
         @test rank == 3
     end
 
@@ -36,15 +36,26 @@ MPI.Init()
         @test a == [1, 5, 9]
     end
 
-    @onrank 1, begin
+    @onrank 1 begin
         @test a == [2, 6, 10]
     end
 
-    @onrank 2, begin
+    @onrank 2 begin
         @test a == [3, 7]
     end
 
-    @onrank 3, begin
+    @onrank 3 begin
         @test a == [4, 8]
     end
+
+    split_comm = MPI.Comm_split(MPI.COMM_WORLD, rank % 2, rank)
+
+    a = Int[]
+    
+    @distribute split_comm for i in 1:10
+        push!(a, i)
+    end
+
+    @onrank 0 @test a == [1, 3, 5, 7, 9]
+    @onrank 1 @test a == [2, 4, 6, 8, 10]
 end
