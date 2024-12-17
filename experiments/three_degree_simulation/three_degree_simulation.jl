c74e6a43f836c6b5dbcf665a7c35cb5664adf8ba
Gregory Wagner <wagner.greg@gmail.com>
2024-12-17 14:42:39 -0700

    Update 3 degree simulation

--- experiments/three_degree_simulation/three_degree_simulation.jl
+++ experiments/three_degree_simulation/three_degree_simulation.jl
@@ -6,11 +6,10 @@ using Oceananigans.Units
 using CFTime
 using Dates
 using Printf
-using CUDA: @allowscalar, device!
 
 using Oceananigans.Grids: znode
 
-arch = GPU()
+arch = CPU()
 
 #####
 ##### Grid and Bathymetry
@@ -22,42 +21,28 @@ Nz = 50
 
 z_faces = exponential_z_faces(; Nz, depth=6000, h=34)
 
-underlying_grid = TripolarGrid(arch;
-                               size = (Nx, Ny, Nz),
-                               z = z_faces,
-                               first_pole_longitude = 70,
-                               north_poles_latitude = 55)
-
-bottom_height = regrid_bathymetry(underlying_grid;
-                                  minimum_depth = 10,
-                                  interpolation_passes = 75,
-                                  major_basins = 1)
-
-grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(tampered_bottom_height))
+underlying_grid = TripolarGrid(arch; size=(Nx, Ny, Nz), z=z_faces)
+bottom_height = regrid_bathymetry(underlying_grid)
+grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom_height))
 
 gm = Oceananigans.TurbulenceClosures.IsopycnalSkewSymmetricDiffusivity(κ_skew=2000, κ_symmetric=2000)
 catke = ClimaOcean.OceanSimulations.default_ocean_closure()
 viscous_closure = Oceananigans.TurbulenceClosures.HorizontalScalarDiffusivity(ν=2000)
-closure = (gm, catke, viscous_closure)
+
+dates = DateTimeProlepticGregorian(1993, 1, 1) : Month(1) : DateTimeProlepticGregorian(1993, 12, 1)
+temperature = ECCOMetadata(:temperature, dates, ECCO4Monthly())
+salinity = ECCOMetadata(:salinity, dates, ECCO4Monthly())
 
 restoring_rate  = 1 / 10days
 mask = LinearlyTaperedPolarMask(southern=(-80, -70), northern=(70, 90))
+FT = ECCORestoring(temperature, grid; mask, rate=restoring_rate)
+FS = ECCORestoring(salinity, grid; mask, rate=restoring_rate)
 
-dates = DateTimeProlepticGregorian(1993, 1, 1) : Month(1) : DateTimeProlepticGregorian(1993, 12, 1)
-temperature = ECCOMetadata(:temperature, arch; dates, version=ECCO4Monthly())
-salinity    = ECCOMetadata(:salinity, arch;    dates, version=ECCO4Monthly())
-
-# inpainting = NearestNeighborInpainting(30) should be enough to fill the gaps near bathymetry
-FT = ECCORestoring(temperature; grid, mask, rate=restoring_rate, inpainting=NearestNeighborInpainting(50))
-FS = ECCORestoring(salinity;    grid, mask, rate=restoring_rate, inpainting=NearestNeighborInpainting(50))
-forcing = (T=FT, S=FS)
-
-momentum_advection = VectorInvariant()
-tracer_advection   = Centered(order=2)
-
-# Should we add a side drag since this is at a coarser resolution?
-ocean = ocean_simulation(grid; momentum_advection, tracer_advection,
-                         closure, forcing,
+ocean = ocean_simulation(grid;
+                         momentum_advection =  VectorInvariant(),
+                         tracer_advection = WENO(order=5),
+                         closure = (gm, catke, viscous_closure),
+                         forcing = (T=FT, S=FT),
                          tracers = (:T, :S, :e))
 
 set!(ocean.model, T=ECCOMetadata(:temperature; dates=first(dates)),
@@ -86,11 +71,12 @@ function progress(sim)
                    prettytime(sim), iteration(sim), prettytime(sim.Δt),
                    umax..., Tmax, Tmin, prettytime(step_time))
 
-     wall_time[] = time_ns()
+    wall_time[] = time_ns()
 
-     return nothing
+    return nothing
 end
 
 add_callback!(simulation, progress, IterationInterval(10))
 
 run!(simulation)
+
