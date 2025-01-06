062f6c0b4fe69d2e49cec6e266845f48d87ab351
Simone Silvestri <silvestri.simone0@gmail.com>
2025-01-06 14:33:49 -0100

    Update time_step_ocean_sea_ice_model.jl

--- src/OceanSeaIceModels/time_step_ocean_sea_ice_model.jl
+++ src/OceanSeaIceModels/time_step_ocean_sea_ice_model.jl
@@ -30,18 +30,15 @@ function time_step!(coupled_model::OceanSeaIceModel, Δt; callbacks=[], compute_
         time_step!(sea_ice)
     end
 
-    @info "Time-stepping the atmosphere"
     # Time step the atmosphere
     # TODO: allow different time-steps for atmosphere and ocean
     time_step!(atmosphere)
 
-    @info "Time-stepping the Ocean"
     # TODO after ice time-step:
     #  - Adjust ocean heat flux if the ice completely melts?
     ocean.Δt = Δt
     time_step!(ocean)
 
-    @info "Computing Fluxes"
     # TODO:
     # - Store fractional ice-free / ice-covered _time_ for more
     #   accurate flux computation?
