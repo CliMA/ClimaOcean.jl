23ff95a3b4c2eb6f2be4d93e8d74808f857580ec
Simone Silvestri <silvestri.simone0@gmail.com>
2025-02-17 13:42:05 +0100

    bugfix

--- examples/generate_surface_fluxes.jl
+++ examples/generate_surface_fluxes.jl
@@ -68,7 +68,7 @@ coupled_model = OceanSeaIceModel(ocean; atmosphere, radiation=Radiation())
 # # Now that the surface fluxes are computed, we can extract and visualize them.
 # # The turbulent fluxes are stored in `coupled_modelinterfaces.atmosphere_ocean_interface.fluxes`.
 
-fluxes  = coupled_modelinterfaces.atmosphere_ocean_interface.fluxes
+fluxes  = coupled_model.interfaces.atmosphere_ocean_interface.fluxes
 λ, φ, z = nodes(fluxes.sensible_heat)
 
 fig = Figure(size = (800, 800), fontsize = 15)
