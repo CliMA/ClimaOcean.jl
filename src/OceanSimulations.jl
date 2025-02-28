11b3df63f62874b3b48acf8c9d5e66f875408b54
Gregory L. Wagner <wagner.greg@gmail.com>
2025-02-28 07:03:17 -0700

    (0.4.2) Remove ActiveCellEnstrophyConserving from OceanSimulations (#357)
    * Remove ActiveCellEnstrophyConserving
    
    * bump patch release
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/OceanSimulations.jl
+++ src/OceanSimulations.jl
@@ -9,7 +9,6 @@ using Oceananigans.Grids: architecture
 using Oceananigans.Advection: FluxFormAdvection
 using Oceananigans.DistributedComputations: DistributedGrid, all_reduce
 using Oceananigans.BoundaryConditions: DefaultBoundaryCondition
-using Oceananigans.Coriolis: ActiveCellEnstrophyConserving
 using Oceananigans.ImmersedBoundaries: immersed_peripheral_node, inactive_node, MutableGridOfSomeKind
 using OrthogonalSphericalShellGrids
 
