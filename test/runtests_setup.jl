79f252e7fd7118598342b59323d47dc24f64a4e3
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-02 21:58:33 -0700

    Fix problem in runtests setup?

--- test/runtests_setup.jl
+++ test/runtests_setup.jl
@@ -45,5 +45,6 @@ function download_bathymetry(; dir = download_bathymetry_cache,
 end
 
 # Trigger downloading JRA55
+arch = first(test_architectures)
 atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(41))
 
