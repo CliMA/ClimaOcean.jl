7663b967f669998fcd8071ec3969331109ee5118
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-04 11:25:48 +0100

    go ahead

--- src/Bathymetry.jl
+++ src/Bathymetry.jl
@@ -126,10 +126,7 @@ function regrid_bathymetry(target_grid;
     close(dataset)
 
     # Diagnose target grid information
-    arch = architecture(target_grid)
-    φ₁, φ₂ = y_domain(target_grid)
-    λ₁, λ₂ = x_domain(target_grid)
-
+    arch   = architecture(target_grid)
     λ_data = λ_data |> Array{BigFloat}
     φ_data = φ_data |> Array{BigFloat}
     
@@ -205,8 +202,8 @@ function interpolate_bathymetry_in_passes(native_z, target_grid;
     end
  
     # Interpolate in passes
-    latitude  = y_domain(target_grid)
-    longitude = x_domain(target_grid)
+    latitude  = y_domain(native_z.grid)
+    longitude = x_domain(native_z.grid)
 
     ΔNλ = floor((Nλn - Nλt) / passes)
     ΔNφ = floor((Nφn - Nφt) / passes)
