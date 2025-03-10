2dfcaacb3c94a298e3c24c225de41c31ad9cc480
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-10 08:22:27 +0100

    small example

--- experiments/ocean_forced_by_tides.jl
+++ experiments/ocean_forced_by_tides.jl
@@ -1,11 +1,14 @@
 
 using ClimaOcean
 using Oceananigans
+using Oceananigans.BoundaryConditions: FieldBoundaryConditions
+using Oceananigans.Grids
 using OrthogonalSphericalShellGrids
 using Oceananigans.Units
 using Printf
 
 Φ = FieldTimeSeries("tidal_potential_jra55.jld2", "Φ")
+Φ = FieldTimeSeries("tidal_potential_jra55.jld2", "Φ"; boundary_conditions=FieldBoundaryConditions(Φ.grid, (Center, Center, Nothing)))
 Oceananigans.BoundaryConditions.fill_halo_regions!(Φ)
 
 import Oceananigans.BuoyancyFormulations: ρ′ 
@@ -22,11 +25,13 @@ end
 # all passive tracers
 equation_of_state = ZeroBuoyancy(1026.0)
 
+z = MutableVerticalDiscretization([-100, 0])
+
 # A barotropic ocean
-grid = TripolarGrid(size = (700, 400, 1), halo = (7, 7, 7), z = (-10, 0))
+grid = TripolarGrid(; size = (700, 400, 1), halo = (7, 7, 7), z)
 bottom_height = regrid_bathymetry(grid)
 grid  = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map=true)
-ocean = ocean_simulation(grid; closure=nothing, momentum_advection=nothing, bottom_drag_coefficient=0, tracer_advection=nothing, equation_of_state)
+ocean = ocean_simulation(grid; closure=nothing, tracer_advection=nothing, equation_of_state)
 
 # A prescribed tidal forcing
 atmos = PrescribedAtmosphere(Φ.grid, Φ.times; tidal_potential=Φ)
@@ -51,7 +56,7 @@ interfaces = ComponentInterfaces(atmos, ocean, nothing;
                                  radiation)
 
 barotropic_earth_model = OceanSeaIceModel(ocean, nothing; atmosphere=atmos, radiation, interfaces)
-barotropic_earth = Simulation(barotropic_earth_model, Δt=20minutes, stop_time=2days, stop_iteration=1) 
+barotropic_earth = Simulation(barotropic_earth_model, Δt=20minutes, stop_time=360days) 
 
 wall_time = Ref(0.0)
 
@@ -69,6 +74,48 @@ function progress(barotropic_earth)
     wall_time[] = time_ns()
 end
 
+ocean.output_writers[:velocities] = JLD2OutputWriter(ocean.model, ocean.model.velocities;
+                                                     filename="barotropic_earth_velocities.jld2",
+                                                     overwrite_existing=true,
+                                                     schedule=IterationInterval(40))
+
+ocean.output_writers[:free_surface] = JLD2OutputWriter(ocean.model, (; η = ocean.model.free_surface.η);
+                                                       filename="barotropic_earth_free_surface.jld2",
+                                                       overwrite_existing=true,
+                                                       schedule=IterationInterval(40))
+
 add_callback!(barotropic_earth, progress, IterationInterval(100))
 
 run!(barotropic_earth)
+
+u = FieldTimeSeries("barotropic_earth_velocities.jld2", "u"; backend=InMemory(100))
+v = FieldTimeSeries("barotropic_earth_velocities.jld2", "v"; backend=InMemory(100))
+# η = FieldTimeSeries("barotropic_earth_free_surface.jld2", "η")
+# w = FieldTimeSeries("barotropic_earth_velocities.jld2", "w")
+
+using GLMakie, JLD2
+
+file = jldopen("barotropic_earth_free_surface.jld2")
+iters = keys(file["timeseries/t"])
+
+fig = Figure()
+axu = Axis(fig[1, 1], title = "u-velocity")
+axv = Axis(fig[1, 2], title = "v-velocity")
+axη = Axis(fig[2, 1], title = "free surface")
+#axw = Axis(fig[1, 3], title = "w-velocity")
+
+iter = Observable(1)
+
+un = @lift(interior(u[$iter], :, :, 1))
+vn = @lift(interior(v[$iter], :, :, 1))
+ηn = @lift(file["timeseries/η/" * iters[$iter]][:, :, 1])
+# wn = @lift(interior(w[$iter], :, :, 2))
+
+heatmap!(axu, un, colorrange=(-0.1, 0.1))
+heatmap!(axv, vn, colorrange=(-0.1, 0.1))
+heatmap!(axη, ηn, colorrange=(-0.7, 0.7))
+
+record(fig, "tides.mp4", 1:length(u.times), framerate = 8) do i
+    iter[] = i
+    @info "doing iter $i"
+end
