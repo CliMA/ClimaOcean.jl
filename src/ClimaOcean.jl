bfaad59cfdd51d47cdfe2bc84d2668cb1da5387c
Gregory Wagner <wagner.greg@gmail.com>
2025-03-10 17:27:18 -0700

    Try adding a precompile workload

--- src/ClimaOcean.jl
+++ src/ClimaOcean.jl
@@ -96,5 +96,19 @@ using ClimaOcean.OceanSeaIceModels: PrescribedAtmosphere
 using ClimaOcean.DataWrangling.JRA55: JRA55PrescribedAtmosphere, JRA55NetCDFBackend
 using ClimaOcean.DataWrangling.ECCO
 
+using PrecompileTools: @setup_workload, @compile_workload
+using OrthogonalSphericalShellGrids
+
+@setup_workload begin
+    Nx, Ny, Nz = 32, 32, 10
+    @compile_workload begin
+        z = exponential_z_faces(Nz=Nz, depth=6000, h=34)
+        grid = OrthogonalSphericalShellGrids.TripolarGrid(CPU(); size=(Nx, Ny, Nz), halo=(7, 7, 7), z)
+        grid = ImmersedBoundaryGrid(grid, GridFittedBottom((x, y) -> -5000))
+        ocean = ocean_simulation(grid)
+        model = OceanSeaIceModel(ocean)
+    end
+end
+
 end # module
 
