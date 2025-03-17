490951704921d63bc876b6d026db3720a1fe497b
Taimoor Sohail <taimoorsohail@gmail.com>
2025-03-17 16:26:45 +1100

    Adding additional diagnostics to the averaging_MWE.jl

--- examples/averaging_mwe.jl
+++ examples/averaging_mwe.jl
@@ -34,7 +34,7 @@ atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(41))
 
 coupled_model = OceanSeaIceModel(ocean; atmosphere, radiation)
 
-simulation = Simulation(coupled_model; Δt=30minutes, stop_time=30days)
+simulation = Simulation(coupled_model; Δt=30minutes, stop_time=10days)
 
 wall_time = Ref(time_ns())
 
@@ -66,38 +66,60 @@ simulation.callbacks[:progress] = Callback(progress, TimeInterval(1days))
 outputs = merge(ocean.model.tracers, ocean.model.velocities)
 
 avg = []
+avg_z = []
 int = []
 
 ρₒ = simulation.model.interfaces.ocean_properties.reference_density
 cₚ = simulation.model.interfaces.ocean_properties.heat_capacity
-
+S₀ = 35 #g/kg
 
 for key in keys(ocean.model.tracers)
-    push!(avg, Field(Average(ocean.model.tracers[key])))
-    push!(int, Field(Integral(ocean.model.tracers[key])))
+    push!(avg, (Average(ocean.model.tracers[key])))
+    push!(avg_z, (Average(ocean.model.tracers[key]), dims = (1,2)))
+    push!(int, (Integral(ocean.model.tracers[key]), dims = (1,2)))
 end
+
+for key in keys(ocean.model.velocities)
+    push!(avg_z, (Average(ocean.model.velocities[key]), dims = (1,2)))
+end
+
 c = CenterField(grid)
 volmask =  set!(c, 1)
 
-dv = Field(Integral(volmask))
+dv = (Integral(volmask))
 
+# Globally Averaged tracers
 avg_tracer_outputs = merge((; T_avg=avg[1], S_avg=avg[2], e_avg=avg[3]))
-int_tracer_outputs = merge((; OHC=ρₒ*cₚ*int[1], S_int=int[2], e_int=int[3], vol_int = dv))
+# Depth-integrated tracers
+int_tracer_outputs = merge((; OHC=ρₒ*cₚ*int[1], S_int=(ρₒ/S₀)*int[2], e_int=int[3], vol_int = dv))
+
+depth_avg_tracer_outputs = merge((; T_avgz=avg_z[1], S_avgz=avg_z[2], e_avgz=avg_z[3]))
+depth_avg_velocity_outputs = merge((; u_avgz=avg_z[4], v_avgz=avg_z[5], w_avgz=avg_z[6]))
+depth_avg_outputs = merge(depth_avg_tracer_outputs, depth_avg_velocity_outputs)
+
+
+simulation.output_writers[:surface] = JLD2OutputWriter(ocean.model, outputs;
+                                                  schedule = TimeInterval(5days),
+                                                  filename = "global_surface_fields",
+                                                  indices = (:, :, grid.Nz),
+                                                  with_halos = true,
+                                                  overwrite_existing = true,
+                                                  array_type = Array{Float32})
 
 simulation.output_writers[:global_avg] = JLD2OutputWriter(ocean.model, avg_tracer_outputs;
                                                   schedule = TimeInterval(1days),
                                                   filename = "averaged_tracer_data",
-                                                  overwrite_existing = true)#=
-                                                  indices = (:, :, grid.Nz),
-                                                  with_halos = true,
-                                                  ,
-                                                  array_type = Array{Float32})
-=#
+                                                  overwrite_existing = true)
+
+simulation.output_writers[:global_depth_avg] = JLD2OutputWriter(ocean.model, depth_avg_tracer_outputs;
+                                                  schedule = TimeInterval(1days),
+                                                  filename = "depth_averaged_tracer_data",
+                                                  overwrite_existing = true)
+
 
 simulation.output_writers[:global_int] = JLD2OutputWriter(ocean.model, int_tracer_outputs;
                                                   schedule = TimeInterval(1days),
-                                                  filename = "integrated_tracer_data",
+                                                  filename = "integrated_tripolar_data",
                                                   overwrite_existing = true)
 
 run!(simulation)
-
