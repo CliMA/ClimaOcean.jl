d034d9bd663594c3536fe560850827b9dc6602a7
Taimoor Sohail <taimoorsohail@gmail.com>
2025-03-19 13:42:47 +1100

    Some tests

---
+++ examples/generate_atmos_dataset.jl
@@ -0,0 +1,17 @@
+using ClimaOcean
+using Oceananigans
+using JLD2
+
+time_indices = 1:1
+
+qt = ClimaOcean.JRA55.JRA55_field_time_series(:specific_humidity; time_indices)
+Tt = ClimaOcean.JRA55.JRA55_field_time_series(:temperature; time_indices)
+pt = ClimaOcean.JRA55.JRA55_field_time_series(:sea_level_pressure; time_indices)
+
+Nx, Ny, Nz = size(qt[1])
+
+q = Array(interior(qt[1], 1:4:Nx, 1:4:Ny, 1))
+T = Array(interior(Tt[1], 1:4:Nx, 1:4:Ny, 1))
+p = Array(interior(pt[1], 1:4:Nx, 1:4:Ny, 1))
+
+@save "atmospheric_state.jld2" q T p
