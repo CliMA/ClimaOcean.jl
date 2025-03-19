1212f25c7e069388ff724c05cb0dbc4e0ecb2deb
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-03-20 07:28:59 +1100

    code alignment

--- examples/single_column_os_papa_simulation.jl
+++ examples/single_column_os_papa_simulation.jl
@@ -166,8 +166,8 @@ outputs = merge(fields, fluxes)
 filename = "single_column_omip_$(location_name)"
 
 simulation.output_writers[:jld2] = JLD2Writer(ocean.model, outputs; filename,
-                                                    schedule = TimeInterval(3hours),
-                                                    overwrite_existing = true)
+                                              schedule = TimeInterval(3hours),
+                                              overwrite_existing = true)
 
 run!(simulation)
 
