ab442c8755e708e08f9571b5db86653b2e972ab2
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-26 08:37:25 -0600

    Use separate representations of the barotropic potential for u,v

--- src/OceanSeaIceModels/InterfaceComputations/interpolate_atmospheric_state.jl
+++ src/OceanSeaIceModels/InterfaceComputations/interpolate_atmospheric_state.jl
@@ -110,10 +110,12 @@ function interpolate_atmosphere_state!(interfaces, atmosphere::PrescribedAtmosph
     end
 
     # Set ocean barotropic pressure forcing
-    barotropic_potential = forcing_barotropic_potential(ocean)
+    u_potential = forcing_barotropic_potential(ocean.model.forcing.u)
+    v_potential = forcing_barotropic_potential(ocean.model.forcing.v)
     ρₒ = coupled_model.interfaces.ocean_properties.reference_density
     if !isnothing(barotropic_potential)
-        parent(barotropic_potential) .= parent(atmosphere_data.p) ./ ρₒ
+        parent(u_potential) .= parent(atmosphere_data.p) ./ ρₒ
+        parent(v_potential) .= parent(atmosphere_data.p) ./ ρₒ
     end
 end
 
