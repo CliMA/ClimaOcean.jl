4bd5a06962c7b057159fd0fe951a09faed063ebf
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-31 10:21:17 +0200

    bugfix

--- src/OceanSeaIceModels/InterfaceComputations/interface_states.jl
+++ src/OceanSeaIceModels/InterfaceComputations/interface_states.jl
@@ -227,11 +227,11 @@ end
     # Calculating the atmospheric temperature
     # We use to compute the sensible heat flux 
     Tₐ = surface_atmosphere_temperature(Ψₐ, ℙₐ)
-    ΔT = Tₐ - Ψₛ.Tᵢ
+    ΔT = Tₐ - Ψₛ.T
     Ωc = ifelse(ΔT == 0, zero(h), Qc / ΔT * λ) # Sensible heat transfer coefficient (W/m²K)
 
     # Computing the flux balance temperature
-    return (Ψᵢ.Tᵢ * F.κ - (Jᵀ + Ωc * Tₐ) * F.δ) / (F.κ - Ωc * F.δ)
+    return (Ψᵢ.T * F.κ - (Jᵀ + Ωc * Tₐ) * F.δ) / (F.κ - Ωc * F.δ)
 end
 
 # Qv + Qu + Qd + Ωc * (Tₐ - Tˢ) + k / h * (Tˢ - Tᵢ) = 0
