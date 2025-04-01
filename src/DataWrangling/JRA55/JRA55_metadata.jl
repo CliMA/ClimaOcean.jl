4fc95cc17839bfd0c55c1b8bba57b0b2bde6a15c
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-01 09:22:17 +0200

    Update JRA55_metadata.jl

--- src/DataWrangling/JRA55/JRA55_metadata.jl
+++ src/DataWrangling/JRA55/JRA55_metadata.jl
@@ -17,7 +17,7 @@ import ClimaOcean.DataWrangling: all_dates, metadata_filename, download_dataset,
 struct JRA55MultipleYears end
 struct JRA55RepeatYear end
 
-const JRA55Metadata{D} = Metadata{<:Union{<:JRA55MultipleYears, <:JRA55RepeatYear}, D} where {D}
+const JRA55Metadata{D} = Metadata{<:Union{<:JRA55MultipleYears, <:JRA55RepeatYear}, D}
 const JRA55Metadatum   = Metadatum{<:Union{<:JRA55MultipleYears, <:JRA55RepeatYear}}
 
 default_download_directory(::Union{<:JRA55MultipleYears, <:JRA55RepeatYear}) = download_JRA55_cache
