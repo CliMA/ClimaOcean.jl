83cb0a587c48b9af647370b838fb8a533c1f77e1
Gregory L. Wagner <wagner.greg@gmail.com>
2025-04-12 14:37:26 -0400

    Update component_interfaces.jl

--- src/OceanSeaIceModels/InterfaceComputations/component_interfaces.jl
+++ src/OceanSeaIceModels/InterfaceComputations/component_interfaces.jl
@@ -121,7 +121,6 @@ function atmosphere_exchanger(atmosphere::PrescribedAtmosphere, exchange_grid)
     TX, TY, TZ = topology(exchange_grid)
     fi = TX() isa Flat ? nothing : Field{Center, Center, Nothing}(exchange_grid, FT)
     fj = TY() isa Flat ? nothing : Field{Center, Center, Nothing}(exchange_grid, FT)
-    # frac_indices = (i=fi, j=fj) # no k needed, only horizontal interpolation
     frac_indices = PrescribedAtmosphereExchanger(i, j)
 
     return frac_indices
