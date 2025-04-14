93d054d117180766f7deae2da2cb6c924d9a8e6e
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-14 10:29:29 +0200

    comments

--- ext/ClimaOceanSpeedyWeatherExt/speedy_weather_exchanger.jl
+++ ext/ClimaOceanSpeedyWeatherExt/speedy_weather_exchanger.jl
@@ -41,7 +41,7 @@ function atmosphere_exchanger(atmosphere::SpeedySimulation, exchange_grid, excha
 
     regridder = ConservativeRegridding.intersection_areas(polys1, polys2)
 
-    exchange_areas = vec(sum(A, dims=2))
+    exchange_areas   = vec(sum(A, dims=2))
     atmosphere_areas = vec(sum(A, dims=1))
 
     exchanger = (; cpu_surface_state, regridder, exchange_areas, atmosphere_areas)
@@ -51,44 +51,45 @@ end
 
 fill_exchange_fields!(::Oceananigans.CPU, args...) = nothing
 
+# TODO: add GPU support
 function fill_exchange_fields!(::Oceananigans.GPU, exchange_state, cpu_surface_state)
     # Can I just copyto! here?
 end
 
+# Regrid the atmospheric state on the exchange grid
 function interpolate_atmosphere_state!(interfaces, atmos::SpeedySimulation, coupled_model)
-    # Get the atmospheric state on the ocean grid
     exchanger = interfaces.exchanger
     regridder = exchanger.regridder
     exchange_grid = interfaces.exchanger.exchange_grid
     exchange_state = exchanger.exchange_atmosphere_state
-    arch = architecture(exchange_grid)
 
-    ua = atmos.diagnostic_variables.grid.u_grid[:, end]           
-    va = atmos.diagnostic_variables.grid.v_grid[:, end]           
-    Ta = atmos.diagnostic_variables.grid.temp_grid[:, end]        
-    qa = atmos.diagnostic_variables.grid.humid_grid[:, end]       
-    pa = exp.(atmos.diagnostic_variables.grid.pres_grid[:, end])  
+    ua  = atmos.diagnostic_variables.grid.u_grid[:, end]           
+    va  = atmos.diagnostic_variables.grid.v_grid[:, end]           
+    Ta  = atmos.diagnostic_variables.grid.temp_grid[:, end]        
+    qa  = atmos.diagnostic_variables.grid.humid_grid[:, end]       
+    pa  = exp.(atmos.diagnostic_variables.grid.pres_grid[:, end])  
     Qsa = atmos.diagnostic_variables.physics.surface_shortwave_down
     Qla = atmos.diagnostic_variables.physics.surface_longwave_down 
 
     exchange_areas = exchanger.exchange_areas
 
-    ue = exchanger.cpu_surface_state.u
-    ve = exchanger.cpu_surface_state.v
-    Te = exchanger.cpu_surface_state.T
-    qe = exchanger.cpu_surface_state.q
-    pe = exchanger.cpu_surface_state.p
+    ue  = exchanger.cpu_surface_state.u
+    ve  = exchanger.cpu_surface_state.v
+    Te  = exchanger.cpu_surface_state.T
+    qe  = exchanger.cpu_surface_state.q
+    pe  = exchanger.cpu_surface_state.p
     Qse = exchanger.cpu_surface_state.Qs
     Qle = exchanger.cpu_surface_state.Qℓ
 
-    regrid!(ua, ue, regridder, exchange_areas)
-    regrid!(va, ve, regridder, exchange_areas)
-    regrid!(Ta, Te, regridder, exchange_areas)
-    regrid!(qa, qe, regridder, exchange_areas)
-    regrid!(pa, pe, regridder, exchange_areas)
+    regrid!(ua,  ue,  regridder, exchange_areas)
+    regrid!(va,  ve,  regridder, exchange_areas)
+    regrid!(Ta,  Te,  regridder, exchange_areas)
+    regrid!(qa,  qe,  regridder, exchange_areas)
+    regrid!(pa,  pe,  regridder, exchange_areas)
     regrid!(Qsa, Qse, regridder, exchange_areas)
     regrid!(Qla, Qle, regridder, exchange_areas)
     
+    arch = architecture(exchange_grid)
     fill_exchange_fields!(arch, exchange_state, exchanger.cpu_surface_state)
 
     return nothing
