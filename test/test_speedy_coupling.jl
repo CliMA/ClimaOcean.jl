269ca7676b8af6196cb1aeb55af7c4af0265d2b5
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-15 12:49:22 +0200

    still some NaNs to fix

--- test/test_speedy_coupling.jl
+++ test/test_speedy_coupling.jl
@@ -7,13 +7,16 @@ using Test
 ClimaOceanSpeedyWeatherExt = Base.get_extension(ClimaOcean, :ClimaOceanSpeedyWeatherExt)
 @test !isnothing(ClimaOceanSpeedyWeatherExt)
 
-spectral_grid = SpeedyWeather.SpectralGrid(trunc=31, nlayers=10)
+spectral_grid = SpeedyWeather.SpectralGrid(trunc=51, nlayers=3)
 oceananigans_grid = LatitudeLongitudeGrid(Oceananigans.CPU(); size=(200, 100, 1), latitude=(-80, 80), longitude=(0, 360), z = (0, 1))
 
 ocean = ClimaOcean.OceanSimulations.ocean_simulation(oceananigans_grid; momentum_advection=nothing, tracer_advection=nothing, closure=nothing)
 atmos = ClimaOcean.atmosphere_simulation(spectral_grid)
-earth = OceanSeaIceModel(ocean; atmosphere=atmos)
 
+# We initialize the atmosphere simulation with some speed and some temperature
+SpeedyWeather.initialize!(atmos)
+
+earth = OceanSeaIceModel(ocean; atmosphere=atmos)
 fluxes = earth.interfaces.atmosphere_ocean_interface.fluxes
 
 Oceananigans.set!(fluxes.sensible_heat, (x, y) -> exp(- y^2 / 10^2))
