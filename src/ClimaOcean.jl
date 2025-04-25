8bd0d3608562225139e183bbfbe0e3101798e698
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-04-25 11:31:15 +1000

    Update ClimaOcean.jl

--- src/ClimaOcean.jl
+++ src/ClimaOcean.jl
@@ -99,15 +99,9 @@ using PrecompileTools: @setup_workload, @compile_workload
 @setup_workload begin
     Nx, Ny, Nz = 32, 32, 10
     @compile_workload begin
-<<<<<<< HEAD
-        # z = exponential_z_faces(Nz=Nz, depth=6000, h=34)
-        # grid = Oceananigans.OrthogonalSphericalShellGrids.TripolarGrid(CPU(); size=(Nx, Ny, Nz), halo=(7, 7, 7), z)
-        # grid = ImmersedBoundaryGrid(grid, GridFittedBottom((x, y) -> -5000))
-=======
         z = exponential_z_faces(Nz=Nz, depth=6000, h=34)
         grid = Oceananigans.OrthogonalSphericalShellGrids.TripolarGrid(CPU(); size=(Nx, Ny, Nz), halo=(7, 7, 7), z)
         grid = ImmersedBoundaryGrid(grid, GridFittedBottom((x, y) -> -5000))
->>>>>>> main
         # ocean = ocean_simulation(grid)
         # model = OceanSeaIceModel(ocean)
     end
