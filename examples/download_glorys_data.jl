3af72ef85f8971a11fc1218fe3361c7f94e1d777
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-04-28 17:56:43 -0600

    add simple example

--- examples/download_glorys_data.jl
+++ examples/download_glorys_data.jl
@@ -1,13 +1,29 @@
 using ClimaOcean
+using Oceananigans
 using PythonCall
 
-dataset = ClimaOcean.DataWrangling.Copernicus.GLORYSDaily()
-u_meta = ClimaOcean.DataWrangling.Metadatum(:u_velocity; dataset)
-v_meta = ClimaOcean.DataWrangling.Metadatum(:v_velocity; dataset)
-T_meta = ClimaOcean.DataWrangling.Metadatum(:temperature; dataset)
-S_meta = ClimaOcean.DataWrangling.Metadatum(:salinity; dataset)
+dataset = ClimaOcean.DataWrangling.Copernicus.GLORYSStatic()
+static_meta = ClimaOcean.DataWrangling.Metadatum(:depth; dataset)
+coords_path = ClimaOcean.DataWrangling.download_dataset(static_meta)
+@info "Downloaded coordinates data to $coords_path"
 
-ClimaOcean.DataWrangling.download_dataset(u_meta)
-ClimaOcean.DataWrangling.download_dataset(v_meta)
-ClimaOcean.DataWrangling.download_dataset(T_meta)
-ClimaOcean.DataWrangling.download_dataset(S_meta)
+dataset = ClimaOcean.DataWrangling.Copernicus.GLORYSDaily()
+T_meta = ClimaOcean.DataWrangling.Metadatum(:temperature; dataset)
+T_path = ClimaOcean.DataWrangling.download_dataset(T_meta)
+@info "Downloaded temperature data to $T_path"
+T = Field(T_meta)
+
+u_meta = ClimaOcean.DataWrangling.Metadatum(:u_velocity; dataset)
+u_path = ClimaOcean.DataWrangling.download_dataset(u_meta)
+@info "Downloaded u velocity data to $u_path"
+u = Field(u_meta)
+
+v_meta = ClimaOcean.DataWrangling.Metadatum(:v_velocity; dataset)
+v_path = ClimaOcean.DataWrangling.download_dataset(v_meta)
+@info "Downloaded data to $v_path"
+v = Field(v_meta)
+
+S_meta = ClimaOcean.DataWrangling.Metadatum(:salinity; dataset)
+S_path = ClimaOcean.DataWrangling.download_dataset(S_meta)
+@info "Downloaded data to $S_path"
+S = Field(S_meta)
