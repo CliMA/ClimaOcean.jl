59fad88c480f437188ff75cd4cbf0acedfc5c13a
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-05-02 11:30:29 -0600

    fix

--- test/test_copernicus_downloading.jl
+++ test/test_copernicus_downloading.jl
@@ -3,6 +3,7 @@ include("runtests_setup.jl")
 @testset "Downloading Copernicus data" begin
     variables = (:temperature, :salinity, :u_velocity, :v_velocity)
     bounding_box = ClimaOcean.DataWrangling.BoundingBox(longitude=(200, 202), latitude=(35, 37))
+    dataset = ClimaOcean.DataWrangling.Copernicus.GLORYSDaily()
     for variable in variables
         metadata = Metadata(variable; dataset, bounding_box)
         filepath = ClimaOcean.DataWrangling.metadata_path(metadata)
