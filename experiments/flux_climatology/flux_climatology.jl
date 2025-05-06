894ab394cd88e1a26b04fdd08678595ced1c2bbb
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-05-06 14:16:54 +0300

    add Qc+Qv + better averaging that do not depend on the end_time

--- experiments/flux_climatology/flux_climatology.jl
+++ experiments/flux_climatology/flux_climatology.jl
@@ -19,60 +19,71 @@ import Oceananigans.Models: timestepper, update_model_field_time_series!
 import ClimaOcean.OceanSeaIceModels: reference_density, heat_capacity
 import Oceananigans.Architectures: on_architecture
 
+ClimaOcean.DataWrangling.dataset_defaults.FloatType = Float64
+
 #####
 ##### A Data structure that holds flux statistics
 #####
 
 struct FluxStatistics{F}
-    avg :: F
+    mean :: F
+    meansq :: F
     std :: F
     max :: F
     min :: F
 end
 
 function FluxStatistics(f::Field)
-    a = similar(f)
-    s = similar(f)
-    p = similar(f)
-    m = similar(f)
+    mean = similar(f)
+    meansq = similar(f)
+    std = similar(f)
+    max = similar(f)
+    min = similar(f)
 
-    fill!(a, 0)
-    fill!(s, 0)
-    fill!(p, 0)
-    fill!(m, 0)
+    fill!(mean, 0)
+    fill!(meansq, 0)
+    fill!(std, 0)
+    fill!(max, 0)
+    fill!(min, 0)
 
-    return FluxStatistics(a, s, p, m)
+    return FluxStatistics(mean, meansq, std, max, min)
 end
 
-Adapt.adapt_structure(to, f::FluxStatistics) = FluxStatistics(Adapt.adapt(to, f.avg),
+compute_std!(f::FluxStatistics) = parent(f.std) .= sqrt.(parent(f.meansq) .- parent(f.mean).^2)
+
+Adapt.adapt_structure(to, f::FluxStatistics) = FluxStatistics(Adapt.adapt(to, f.mean),
+                                                              Adapt.adapt(to, f.meansq),
                                                               Adapt.adapt(to, f.std),
                                                               Adapt.adapt(to, f.max),
                                                               Adapt.adapt(to, f.min))
 
-on_architecture(arch, f::FluxStatistics) = FluxStatistics(on_architecture(arch, f.avg),
+on_architecture(arch, f::FluxStatistics) = FluxStatistics(on_architecture(arch, f.mean),
+                                                          on_architecture(arch, f.meansq),
                                                           on_architecture(arch, f.std),
                                                           on_architecture(arch, f.max),
                                                           on_architecture(arch, f.min))
 
-function update_stats!(stats::FluxStatistics, flux, Δt, stop_time)
+function update_stats!(stats::FluxStatistics, flux, iteration)
     grid = flux.grid
     arch = architecture(grid)
-    launch!(arch, grid, :xy, _update_stats!, stats, flux, Δt, stop_time)
+    launch!(arch, grid, :xy, _update_stats!, stats, flux, iteration)
 end
 
-@kernel function _update_stats!(stats, flux, Δt, stop_time)
+@kernel function _update_stats!(stats, flux, iteration)
     i, j = @index(Global, NTuple)
 
+    inverse_iteration = 1 / (iteration + 1)
+
     @inbounds begin
-         f = flux[i, j, 1]
-        stats.avg[i, j, 1] += f * Δt / stop_time
-        stats.std[i, j, 1] += f^2 * Δt / stop_time
+        f = flux[i, j, 1]
+        stats.mean[i, j, 1] *= 1 - inverse_iteration
+        stats.mean[i, j, 1] += f * inverse_iteration
+        stats.meansq[i, j, 1] *= 1 - inverse_iteration
+        stats.meansq[i, j, 1] += f^2 * inverse_iteration
         stats.max[i, j, 1] = max(stats.max[i, j, 1], f)
         stats.min[i, j, 1] = min(stats.min[i, j, 1], f)
     end
 end
-
-finalize_std!(f::FluxStatistics) = parent(f.std) .= sqrt.(parent(f.std) .- parent(f.avg).^2)
 
 #####
 ##### A function to compute flux statistics
@@ -92,13 +103,13 @@ function compute_flux_climatology(earth)
     stats = (; τx, τy, Jᵀ, Jˢ, Qc, Qv)
 
     function update_flux_stats!(earth)
-        Δt = earth.Δt
-        stop_time = earth.stop_time
-
-        update_stats!(τx, net_fluxes.u, Δt, stop_time)
-        update_stats!(τy, net_fluxes.v, Δt, stop_time)
-        update_stats!(Jᵀ, net_fluxes.T, Δt, stop_time)
-        update_stats!(Jˢ, net_fluxes.S, Δt, stop_time)
+        iteration = earth.model.clock.iteration
+        update_stats!(τx, net_fluxes.u, iteration)
+        update_stats!(τy, net_fluxes.v, iteration)
+        update_stats!(Jᵀ, net_fluxes.T, iteration)
+        update_stats!(Jˢ, net_fluxes.S, iteration)
+        update_stats!(Qc, atmos_ocean_fluxes.sensible_heat, iteration)
+        update_stats!(Qv, atmos_ocean_fluxes.latent_heat, iteration)
 
         return nothing
     end
@@ -107,10 +118,12 @@ function compute_flux_climatology(earth)
 
     run!(earth)
 
-    finalize_std!(τx)
-    finalize_std!(τy)
-    finalize_std!(Jᵀ)
-    finalize_std!(Jˢ)
+    compute_std!(τx)
+    compute_std!(τy)
+    compute_std!(Jᵀ)
+    compute_std!(Jˢ)
+    update_stats!(Qc)
+    update_stats!(Qv)
 
     return stats
 end
@@ -147,25 +160,29 @@ end
 
 # ...with prescribed velocity and tracer fields
 dataset = ECCO4Monthly()
-arch    = GPU()
+arch    = CPU()
 
 start_date = DateTime(1992, 1, 1)
-end_date   = DateTime(1993, 1, 1)
+end_date   = DateTime(1992, 12, 31)
 
-T_meta = Metadata(:temperature; start_date, end_date, dataset)
-S_meta = Metadata(:salinity;    start_date, end_date, dataset)
+stop_time = Day(end_date - start_date).value * Oceananigans.Units.days
+
+time_indices_in_memory = 13
+
 u_meta = Metadata(:u_velocity;  start_date, end_date, dataset)
 v_meta = Metadata(:v_velocity;  start_date, end_date, dataset)
+T_meta = Metadata(:temperature; start_date, end_date, dataset)
+S_meta = Metadata(:salinity;    start_date, end_date, dataset)
 
-u = FieldTimeSeries(u_meta, arch; time_indices_in_memory=13)
-v = FieldTimeSeries(v_meta, arch; time_indices_in_memory=13)
-T = FieldTimeSeries(T_meta, arch; time_indices_in_memory=13)
-S = FieldTimeSeries(S_meta, arch; time_indices_in_memory=13)
+u = FieldTimeSeries(u_meta, arch; time_indices_in_memory)
+v = FieldTimeSeries(v_meta, arch; time_indices_in_memory)
+T = FieldTimeSeries(T_meta, arch; time_indices_in_memory)
+S = FieldTimeSeries(S_meta, arch; time_indices_in_memory)
 
 grid = ECCO.ECCO_immersed_grid(arch)
 
 ocean_model = PrescribedOcean((; u, v, T, S); grid)
-ocean = Simulation(ocean_model, Δt=3hours, stop_time=365days)
+ocean = Simulation(ocean_model; Δt=3hours, stop_time)
 
 #####
 ##### Need to extend a couple of methods
@@ -206,34 +223,42 @@ atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(1000))
 ##### A prescribed earth...
 #####
 
-earth_model = OceanSeaIceModel(ocean, nothing; atmosphere, radiation = Radiation(ocean_emissivity=0, ocean_albedo=1))
-earth = Simulation(earth_model, Δt=3hours, stop_time=365days)
+earth_model = OceanSeaIceModel(ocean, nothing; atmosphere, radiation = Radiation(arch))
+earth = Simulation(earth_model; Δt=3hours, stop_time)
 
 wall_time = Ref(time_ns())
 
 # Just checking that the ocean evolves as expected
 function progress(sim)
-    ocean = sim.model.ocean
-    u, v, w = ocean.model.velocities
-    T = ocean.model.tracers.T
+    net_fluxes = sim.model.interfaces.net_fluxes.ocean_surface
+    atmos_ocean_fluxes = sim.model.interfaces.atmosphere_ocean_interface.fluxes
 
-    Tmax = maximum(interior(T))
-    Tmin = minimum(interior(T))
+    Q = net_fluxes.Q
+    Qc = atmos_ocean_fluxes.sensible_heat
+    Qv = atmos_ocean_fluxes.latent_heat
 
-    umax = (maximum(abs, interior(u)),
-            maximum(abs, interior(v)))
+    τx = net_fluxes.u
+    τy = net_fluxes.v
+
+    Qmax = maximum(Q)
+    Qmin = minimum(Q)
+    Qcmax = maximum(Qc)
+    Qcmin = minimum(Qc)
+    Qvmax = maximum(Qv)
+    Qvmin = minimum(Qv)
+    τmax = (maximum(abs, τx), maximum(abs, τy))
 
     step_time = 1e-9 * (time_ns() - wall_time[])
 
-    msg = @sprintf("Iter: %d, time: %s, Δt: %s", iteration(sim), prettytime(sim), prettytime(sim.Δt))
-    msg *= @sprintf(", max|u|: (%.2e, %.2e) m s⁻¹, extrema(T): (%.2f, %.2f) ᵒC, wall time: %s",
-                    umax..., Tmax, Tmin, prettytime(step_time))
+    msg = @sprintf("Iter: %d, time: %s", iteration(sim), prettytime(sim))
+    msg *= @sprintf(", max|τ|: (%.2e, %.2e) m² s⁻², extrema(Q): (%.2f, %.2f) W m⁻², extrema(Qc): (%.2f, %.2f) W m⁻², extrema(Qv): (%.2f, %.2f) W m⁻², wall time: %s",
+                    τmax..., Qmin, Qmax, Qcmin, Qcmax, Qvmin, Qvmax, prettytime(step_time))
 
     @info msg
 
     wall_time[] = time_ns()
 end
 
-add_callback!(earth, progress, IterationInterval(10))
+add_callback!(earth, progress, IterationInterval(16))
 
 stats = compute_flux_climatology(earth)
