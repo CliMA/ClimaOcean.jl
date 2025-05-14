2d61327e1cd1b5c2c18fd72f4ea56762857a148b
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-05-14 09:27:39 -0600

    Add docstring comment about MPI

--- ext/ClimaOceanPythonCallExt.jl
+++ ext/ClimaOceanPythonCallExt.jl
@@ -3,6 +3,7 @@ module ClimaOceanPythonCallExt
 using ClimaOcean
 using CondaPkg
 using PythonCall
+using Oceananigans.DistributedComputations: @root
 
 using Dates: DateTime
 using ClimaOcean.DataWrangling.Copernicus: CopernicusMetadata
@@ -61,7 +62,8 @@ function download_dataset(meta::CopernicusMetadata, grid=nothing; skip_existing 
 
     additional_kw = NamedTuple(name => value for (name, value) in additional_kw)
     kw = merge(kw, datetime_kw, lon_kw, lat_kw, z_kw, additional_kw)
-    toolbox.subset(; kw...)
+
+    @root toolbox.subset(; kw...)
 
     return output_path
 end
