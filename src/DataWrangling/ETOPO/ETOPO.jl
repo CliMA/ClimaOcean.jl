748957ae1a1e3deaac82a262df9a90789e1f9044
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-05-15 11:14:04 +0300

    bit of cleanup

--- src/DataWrangling/ETOPO/ETOPO.jl
+++ src/DataWrangling/ETOPO/ETOPO.jl
@@ -41,30 +41,22 @@ default_download_directory(::ETOPOBathymetry) = download_ETOPO_cache
 reversed_vertical_axis(::ETOPOBathymetry) = true
 longitude_interfaces(::ETOPOBathymetry) = (-180, 180)
 latitude_interfaces(::ETOPOBathymetry) = (-90, 90)
-Base.size(dataset::ETOPOBathymetry, variable) = (21600, 10800, 1)
-Base.size(dataset::ETOPOBathymetry) = (21600, 10800, 1)
+Base.size(::ETOPOBathymetry, variable) = (21600, 10800, 1)
+Base.size(::ETOPOBathymetry) = (21600, 10800, 1)
 
-all_dates(dataset::ETOPOBathymetry) = nothing
-all_dates(dataset::ETOPOBathymetry, variable) = nothing
-function first_date(dataset::ETOPOBathymetry, variable)
-    return nothing
-end
-last_date(dataset::ETOPOBathymetry, variable) = nothing
+all_dates(::ETOPOBathymetry) = nothing
+all_dates(::ETOPOBathymetry, variable) = nothing
+first_date(::ETOPOBathymetry, variable) = nothing
+last_date(::ETOPOBathymetry, variable) = nothing
 
 const ETOPOMetadatum = Metadatum{<:ETOPOBathymetry, <:Any, <:Any}
 
-const ETOPO_url  = "https://www.dropbox.com/scl/fi/6pwalcuuzgtpanysn4h6f/" *
-"ETOPO_2022_v1_60s_N90W180_surface.nc?rlkey=2t7890ruyk4nd5t5eov5768lt&st=yfxsy1lu&dl=0"
+const ETOPO_url = "https://www.dropbox.com/scl/fi/6pwalcuuzgtpanysn4h6f/" *
+    "ETOPO_2022_v1_60s_N90W180_surface.nc?rlkey=2t7890ruyk4nd5t5eov5768lt&st=yfxsy1lu&dl=0"
 
-z_interfaces(::ETOPOMetadatum)= [0,1]
-
-function metadata_url(m::ETOPOMetadatum)
-    return ETOPO_url
-end
-
-function metadata_filename(metadatum::ETOPOMetadatum)
-    return "ETOPO_2022_v1_60s_N90W180_surface.nc"
-end
+z_interfaces(::ETOPOMetadatum)= (0, 1)
+metadata_url(::ETOPOMetadatum) = ETOPO_url
+metadata_filename(metadatum::ETOPOMetadatum) = "ETOPO_2022_v1_60s_N90W180_surface.nc"
 
 function download_dataset(metadatum::ETOPOMetadatum)
     dir = metadatum.dir
@@ -78,7 +70,7 @@ function download_dataset(metadatum::ETOPOMetadatum)
         @info "Downloading ETOPO data: $(metadatum.name) in $(metadatum.dir)..."
         Downloads.download(fileurl, filepath; progress=download_progress)
     end
-    return nothing
+    return filepath
 end
 
 end #module
