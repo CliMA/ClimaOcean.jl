753ba74cfff475b6751e05e6d8b07437a37effb5
Simone Silvestri <silvestri.simone0@gmail.com>
2025-06-12 15:29:27 +0200

    remove free drift for the moment

--- src/SeaIceSimulations.jl
+++ src/SeaIceSimulations.jl
@@ -114,7 +114,6 @@ function sea_ice_dynamics(grid, ocean=nothing;
                                   top_momentum_stress = (u=τua, v=τva),
                                   bottom_momentum_stress = τo,
                                   rheology,
-                                  free_drift,
                                   solver)
 end
 
