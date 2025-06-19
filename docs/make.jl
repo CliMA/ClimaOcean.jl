827d19e978e719e575f5598e03b08d9f286033e9
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-06-19 15:43:23 +1000

    bring back examples

--- docs/make.jl
+++ docs/make.jl
@@ -17,9 +17,9 @@ const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
 to_be_literated = [
-    # "single_column_os_papa_simulation.jl",
-    # "one_degree_simulation.jl",
-    # "near_global_ocean_simulation.jl"
+    "single_column_os_papa_simulation.jl",
+    "one_degree_simulation.jl",
+    "near_global_ocean_simulation.jl"
 ]
 
 for file in to_be_literated
@@ -41,9 +41,9 @@ pages = [
     "Home" => "index.md",
 
     "Examples" => [
-        # "Single-column ocean simulation" => "literated/single_column_os_papa_simulation.md",
-        # "One-degree ocean simulation" => "literated/one_degree_simulation.md",
-        # "Near-global ocean simulation" => "literated/near_global_ocean_simulation.md",
+        "Single-column ocean simulation" => "literated/single_column_os_papa_simulation.md",
+        "One-degree ocean--sea ice simulation" => "literated/one_degree_simulation.md",
+        "Near-global ocean simulation" => "literated/near_global_ocean_simulation.md",
         ],
 
     "Vertical grids" => "vertical_grids.md",
