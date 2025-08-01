5e0997c34260dc1c7720077ffdb90558741e8637
simone-silvestri <silvestri.simone0@gmail.com>
2025-08-01 05:41:12 -0400

    comment out for now

--- src/SeaIceSimulations.jl
+++ src/SeaIceSimulations.jl
@@ -63,11 +63,13 @@ function sea_ice_simulation(grid, ocean=nothing;
     bottom_heat_flux = Field{Center, Center, Nothing}(grid)
     top_heat_flux    = Field{Center, Center, Nothing}(grid)
 
-    immersed_u_bc = FluxBoundaryCondition(u_immersed_bottom_drag, discrete_form=true, parameters=1e-1)
-    immersed_v_bc = FluxBoundaryCondition(v_immersed_bottom_drag, discrete_form=true, parameters=1e-1)
-    
-    u_bcs = FieldBoundaryConditions(grid, (Face, Center, Nothing); immersed = immersed_u_bc)
-    v_bcs = FieldBoundaryConditions(grid, (Center, Face, Nothing); immersed = immersed_v_bc)
+    u_bc = FluxBoundaryCondition(u_immersed_bottom_drag, discrete_form=true, parameters=1e-1)
+    v_bc = FluxBoundaryCondition(v_immersed_bottom_drag, discrete_form=true, parameters=1e-1)
+   
+    # immersed_u_bc = ImmersedBoundaryConditions(top=nothing, bottom=nothing,  west=nothing,  east=nothing, south=u_bc, north=u_bc)
+    # immersed_v_bc = ImmersedBoundaryConditions(top=nothing, bottom=nothing, south=nothing, north=nothing,  west=v_bc,  east=v_bc)
+    # u_bcs = FieldBoundaryConditions(grid, (Face(), Center(), nothing); immersed = immersed_u_bc)
+    # v_bcs = FieldBoundaryConditions(grid, (Center(), Face(), nothing); immersed = immersed_v_bc)
 
     # Build the sea ice model
     sea_ice_model = SeaIceModel(grid;
