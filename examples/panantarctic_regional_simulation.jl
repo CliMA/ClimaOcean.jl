32f4df820eb442f6c3a7b0540b534b4716f1f929
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-08-11 07:06:29 +1000

    run less time

--- examples/panantarctic_regional_simulation.jl
+++ examples/panantarctic_regional_simulation.jl
@@ -145,7 +145,7 @@ nothing #hide
 # Now that the simulation has spun up, we can run it for the full 2 years.
 # We increase the maximum time step size to 10 minutes and let the simulation run for 2 years.
 
-simulation.stop_time = 2*365days
+simulation.stop_time = 60days
 simulation.Δt = 10minutes
 run!(simulation)
 nothing #hide
