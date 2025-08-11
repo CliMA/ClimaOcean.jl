a4941a97dcbe151e117ad0c41b80f97313597ceb
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-08-12 06:09:38 +1000

    dreaded missing comma

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 to_be_literated = [
     "single_column_os_papa_simulation.jl",
     "one_degree_simulation.jl",
-    "near_global_ocean_simulation.jl"
+    "near_global_ocean_simulation.jl",
     "panantarctic_regional_simulation.jl"
 ]
 
