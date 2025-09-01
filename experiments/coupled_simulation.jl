99cefaa9076bf4ad112a6f0904abac736d308243
simone-silvestri <silvestri.simone0@gmail.com>
2025-09-01 08:49:17 -0400

    try high res

--- experiments/coupled_simulation.jl
+++ experiments/coupled_simulation.jl
@@ -10,20 +10,20 @@ using Printf, Statistics, Dates
 ##### The Ocean!!!
 #####
 
-Nx = 360 # Longitudinal direction 
-Ny = 180 # Meridional direction 
-Nz = 60  # Vertical levels
+Nx = 4320 # Longitudinal direction 
+Ny = 2160 # Meridional direction 
+Nz = 10  # Vertical levels
 
-r_faces = ExponentialCoordinate(Nz, -6000, 0)
-grid    = TripolarGrid(CPU(); size=(Nx, Ny, Nz), z=r_faces, halo=(5, 5, 4))
+r_faces = ExponentialCoordinate(Nz, -4000, 0)
+grid    = TripolarGrid(GPU(); size=(Nx, Ny, Nz), z=r_faces, halo=(7, 7, 6))
 
 # Regridding the bathymetry...
 bottom_height = regrid_bathymetry(grid; minimum_depth=15, major_basins=1, interpolation_passes=15)
 grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map=true)
 
 # Advection
-momentum_advection = WENOVectorInvariant(order=5)
-tracer_advection   = WENO(order=5)
+momentum_advection = WENOVectorInvariant()
+tracer_advection   = WENO(order=7)
 
 # Free Surface
 free_surface = SplitExplicitFreeSurface(grid; substeps=80)
@@ -31,7 +31,7 @@ free_surface = SplitExplicitFreeSurface(grid; substeps=80)
 # Parameterizations
 catke_closure = RiBasedVerticalDiffusivity()
 eddy_closure  = Oceananigans.TurbulenceClosures.IsopycnalSkewSymmetricDiffusivity(κ_skew=1e3, κ_symmetric=1e3)
-closures      = (catke_closure, eddy_closure, VerticalScalarDiffusivity(κ=1e-5, ν=1e-4))
+closures      = (catke_closure, VerticalScalarDiffusivity(κ=1e-5, ν=1e-4))
 
 # The ocean simulation
 ocean = ocean_simulation(grid; 
@@ -49,7 +49,7 @@ Oceananigans.set!(ocean.model, T=Metadatum(:temperature, dataset=ECCO4Monthly())
 ##### The Atmosphere!!!
 #####
 
-spectral_grid = SpectralGrid(trunc=63, nlayers=8, Grid=FullClenshawGrid)
+spectral_grid = SpectralGrid(trunc=255, nlayers=8, Grid=FullClenshawGrid)
 
 humidity_flux_ocean = PrescribedOceanHumidityFlux(spectral_grid)
 humidity_flux_land = SurfaceLandHumidityFlux(spectral_grid)
@@ -87,11 +87,16 @@ end
 initialize_atmospheric_state!(atmosphere)
 # atmosphere.model.feedback.verbose = false
 
+atmosphere.model.output.output_dt = Hour(3)
+atmosphere.model.output.active = true
+add!(atmosphere.model, SpeedyWeather.SurfaceFluxesOutput()...)
+add!(atmosphere.model, SpeedyWeather.PrecipitationOutput()...)
+
 #####
 ##### The Sea-Ice!!!
 #####
 
-dynamics = ClimaOcean.SeaIceSimulations.sea_ice_dynamics(grid, ocean)
+dynamics = ClimaOcean.SeaIceSimulations.sea_ice_dynamics(grid, ocean; solver=ClimaSeaIce.SeaIceDynamics.SplitExplicitSolver(100))
 sea_ice = sea_ice_simulation(grid, ocean; dynamics, advection=WENO(order=7))
 
 Oceananigans.set!(sea_ice.model, h=Metadatum(:sea_ice_thickness, dataset=ECCO4Monthly()), 
@@ -106,9 +111,9 @@ Oceananigans.set!(sea_ice.model, h=Metadatum(:sea_ice_thickness, dataset=ECCO4Mo
 # Remember in the future that reflected radiation is computed independently by speedy 
 # so we need to communicate albedo in some way if this reflected radiation is to be
 # absorbed by clouds 
-radiation = Radiation(ocean_emissivity=0, sea_ice_emissivity=0)
+radiation = Radiation(ocean_emissivity=0.1, sea_ice_emissivity=0.1)
 earth_model = OceanSeaIceModel(ocean, sea_ice; atmosphere, radiation)
-earth = Oceananigans.Simulation(earth_model; Δt, stop_time=60days)
+earth = Oceananigans.Simulation(earth_model; Δt, stop_time=360days)
 
 wall_time = Ref(time_ns())
 
@@ -137,7 +142,42 @@ function progress(sim)
      return nothing
 end
 
+outputs = merge(ocean.model.velocities, ocean.model.tracers)
+
+ocean.output_writers[:free_surf] = JLD2OutputWriter(ocean.model, (; η=ocean.model.free_surface.η);
+                                                    overwrite_exisiting=true,
+                                                    schedule=TimeInterval(3600 * 3),
+                                                    filename="ocean_free_surface.jld2")
+
+ocean.output_writers[:surface] = JLD2OutputWriter(ocean.model, outputs;
+                                                  overwrite_exisiting=true,
+                                                  schedule=TimeInterval(3600 * 3),
+                                                  filename="ocean_surface_fields.jld2",
+                                                  indices=(:, :, grid.Nz))
+
+sea_ice.output_writers[:fields] = JLD2OutputWriter(sea_ice.model, Oceananigans.fields(sea_ice.model);
+                                                   overwrite_exisiting=true,
+                                                   schedule=TimeInterval(3600 * 3),
+                                                   filename="sea_ice_fields.jld2")
+
+Qcao = earth.model.interfaces.atmosphere_ocean_interface.fluxes.sensible_heat
+Qvao = earth.model.interfaces.atmosphere_ocean_interface.fluxes.latent_heat
+τxao = earth.model.interfaces.atmosphere_ocean_interface.fluxes.x_momentum
+τyao = earth.model.interfaces.atmosphere_ocean_interface.fluxes.y_momentum
+Qcai = earth.model.interfaces.atmosphere_sea_ice_interface.fluxes.sensible_heat
+Qvai = earth.model.interfaces.atmosphere_sea_ice_interface.fluxes.latent_heat
+τxai = earth.model.interfaces.atmosphere_sea_ice_interface.fluxes.x_momentum
+τyai = earth.model.interfaces.atmosphere_sea_ice_interface.fluxes.y_momentum
+Qoi = earth.model.interfaces.net_fluxes.sea_ice_bottom.heat
+Soi = earth.model.interfaces.net_fluxes.sea_ice_bottom.salt
+fluxes = (; Qcao, Qvao, τxao, τyao, Qcai, Qvai, τxai, τyai, Qoi, Soi)
+
+earth.output_writers[:fluxes] = JLD2OutputWriter(earth.model, fluxes;
+                                                 overwrite_exisiting=true,
+                                                 schedule=TimeInterval(3600 * 3),
+                                                 filename="intercomponent_fluxes.jld2")
+
 # And add it as a callback to the simulation.
-# add_callback!(earth, progress, IterationInterval(10))
+add_callback!(earth, progress, IterationInterval(10))
 
 Oceananigans.run!(earth)
