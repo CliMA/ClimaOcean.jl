805445f4b41686c4e7946c26eeb4bd5403443934
Francis J. Poulin <fpoulin@uwaterloo.ca>
2025-09-10 20:13:29 -0400

    Update make.jl
    Resort to what we have in main, exactly. Surely this must work?

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
 to_be_literated = [
-    "panantarctic_regional_simulation.jl",
+    #"panantarctic_regional_simulation.jl",
     "single_column_os_papa_simulation.jl",
     "one_degree_simulation.jl",
     "near_global_ocean_simulation.jl",
@@ -27,9 +27,10 @@ to_be_literated = [
 for file in to_be_literated
     filepath = joinpath(EXAMPLES_DIR, file)
     withenv("JULIA_DEBUG" => "Literate") do
-        run(`julia --project=docs -e "using Literate;
-            Literate.markdown(\"$filepath\", \"$outputpath\";
-                               flavor=Literate.DocumenterFlavor(), execute=true)"`)
+        Literate.markdown(filepath, OUTPUT_DIR; flavor = Literate.DocumenterFlavor(), execute = true)
+        #run(`julia --project=docs -e "using Literate;
+        #    Literate.markdown(\"$filepath\", \"$outputpath\";
+        #                       flavor=Literate.DocumenterFlavor(), execute=true)"`)
     end
     GC.gc()
     CUDA.reclaim()
@@ -50,7 +51,7 @@ pages = [
         "Single-column ocean simulation" => "literated/single_column_os_papa_simulation.md",
         "One-degree ocean--sea ice simulation" => "literated/one_degree_simulation.md",
         "Near-global ocean simulation" => "literated/near_global_ocean_simulation.md",
-        "Panantarctic regional simulation" => "literated/panantarctic_regional_simulation.md",
+        #"Panantarctic regional simulation" => "literated/panantarctic_regional_simulation.md",
         ],
 
     "Vertical grids" => "vertical_grids.md",
