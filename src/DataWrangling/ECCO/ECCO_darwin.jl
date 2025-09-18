aa70fc079e40c808db9068395a39ca5b6204d728
Jonathan Lauderdale <jonathan.lauderdale@gmail.com>
2025-09-18 16:19:47 -0400

    `concentration_units` docstring

--- src/DataWrangling/ECCO/ECCO_darwin.jl
+++ src/DataWrangling/ECCO/ECCO_darwin.jl
@@ -70,9 +70,13 @@ ECCO_darwin_dataset_variable_names = Dict(
     :dissolved_oxygen               => "O2",
 )
 
-# Set up conversion from the output data to standard units
-# salinity = SALTanom + 35
-# concentrations of biogeochemical tracers are in uL => umol/L in the output files
+"""
+    function concentration_units(metadatum::Metadatum{<:Union{ECCO2DarwinMonthly, ECCO4DarwinMonthly}}) 
+
+Set up conversion from the ECCODarwin output data to standard units
+  -  salinity = SALTanom + 35
+  -  biogeochemical tracer concentrations are in uL => umol/L in the output files
+"""
 function concentration_units(metadatum::Metadatum{<:Union{ECCO2DarwinMonthly, ECCO4DarwinMonthly}}) 
     if dataset_variable_name(metadatum) == "SALTanom"
         return GramPerKilogramMinus35()
