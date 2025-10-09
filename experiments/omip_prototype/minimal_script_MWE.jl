207fcc47005410492b67c54b473c378c18892852
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-10-09 10:36:02 -0700

    a "working version" on google cloud

--- experiments/omip_prototype/minimal_script_MWE.jl
+++ experiments/omip_prototype/minimal_script_MWE.jl
@@ -17,15 +17,18 @@ using Oceananigans.BuoyancyFormulations: buoyancy, buoyancy_frequency
 
 import Oceananigans.OutputWriters: checkpointer_address
 
+CUDA.versioninfo()
+
 ngpus = Int(length(CUDA.devices()))
 @info "$ngpus GPUs"
 # arch = GPU()
 arch = Distributed(GPU(), partition=Partition(1, ngpus), synchronized_communication=true)
+# arch = Distributed(GPU(), partition=Partition(1, ngpus))
 # arch = Distributed(CPU(), partition=Partition(1, 4), synchronized_communication=true)
 @info "Architecture $(arch)"
 
-Nx = 1440 # longitudinal direction 
-Ny = 720 # meridional direction 
+Nx = 2880 # longitudinal direction 
+Ny = 1440 # meridional direction 
 Nz = 100
 
 z_faces = ExponentialDiscretization(Nz, -6000, 0)
@@ -42,13 +45,17 @@ grid = TripolarGrid(arch;
 
 bottom_height = regrid_bathymetry(grid; minimum_depth=15, major_basins=1, interpolation_passes=10)
 grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map=true)
+# grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map=false)
 
 momentum_advection = WENOVectorInvariant()
 tracer_advection   = WENO(order=7)
 
-free_surface = SplitExplicitFreeSurface(grid; substeps = 70)
+free_surface = SplitExplicitFreeSurface(grid; substeps = 200)
+
+using Oceananigans.TurbulenceClosures: RiBasedVerticalDiffusivity
 
 obl_closure = ClimaOcean.OceanSimulations.default_ocean_closure()
+# obl_closure = RiBasedVerticalDiffusivity()
 closure = (obl_closure, VerticalScalarDiffusivity(κ=1e-5, ν=1e-4))
 
 dir = joinpath(homedir(), "forcing_data_1deg_minimal_multi40_backend3")
@@ -107,37 +114,57 @@ using Statistics
 function progress(sim)
     sea_ice = sim.model.sea_ice
     ocean   = sim.model.ocean
-    hmax = maximum(sea_ice.model.ice_thickness)
-    ℵmax = maximum(sea_ice.model.ice_concentration)
-    Tmax = maximum(sim.model.interfaces.atmosphere_sea_ice_interface.temperature)
-    Tmin = minimum(sim.model.interfaces.atmosphere_sea_ice_interface.temperature)
-    # Tmax = maximum(ocean.model.tracers.T)
-    # Tmin = minimum(ocean.model.tracers.T)
-    Smax = maximum(ocean.model.tracers.S)
-    Smin = minimum(ocean.model.tracers.S)
-    umax = maximum(ocean.model.velocities.u)
-    vmax = maximum(ocean.model.velocities.v)
-    wmax = maximum(ocean.model.velocities.w)
+    # hmax = maximum(interior(sea_ice.model.ice_thickness))
+    # ℵmax = maximum(interior(sea_ice.model.ice_concentration))
+    # Tmax = maximum(interior(sim.model.interfaces.atmosphere_sea_ice_interface.temperature))
+    # Tmin = minimum(interior(sim.model.interfaces.atmosphere_sea_ice_interface.temperature))
+    # # Tmax = maximum(ocean.model.tracers.T)
+    # # Tmin = minimum(ocean.model.tracers.T)
+    # Smax = maximum(interior(ocean.model.tracers.S))
+    # Smin = minimum(interior(ocean.model.tracers.S))
+    # umax = maximum(interior(ocean.model.velocities.u))
+    # vmax = maximum(interior(ocean.model.velocities.v))
+    # wmax = maximum(interior(ocean.model.velocities.w))
 
     step_time = 1e-9 * (time_ns() - wall_time[])
 
-    msg1 = @sprintf("time: %s, iteration: %d, Δt: %s, ", prettytime(sim), iteration(sim), prettytime(sim.Δt))
-    msg2 = @sprintf("max(h): %.2e m, max(ℵ): %.2e ", hmax, ℵmax)
-    msg4 = @sprintf("extrema(T): (%.2f, %.2f) ᵒC, ", Tmax, Tmin)
-    msg5 = @sprintf("extrema(S): (%.2f, %.2f) psu, ", Smax, Smin)
-    msg6 = @sprintf("maximum(u): (%.2f, %.2f, %.2f) m/s, ", umax, vmax, wmax)
+    msg0 = @sprintf("local rank: %d, ", arch.local_rank)
+    # msg1 = @sprintf("time: %s, iteration: %d, Δt: %s, ", prettytime(sim), iteration(sim), prettytime(sim.Δt))
+    # msg2 = @sprintf("max(h): %.2e m, max(ℵ): %.2e ", hmax, ℵmax)
+    # msg4 = @sprintf("extrema(T): (%.2f, %.2f) ᵒC, ", Tmax, Tmin)
+    # msg5 = @sprintf("extrema(S): (%.2f, %.2f) psu, ", Smax, Smin)
+    # msg6 = @sprintf("maximum(u): (%.2f, %.2f, %.2f) m/s, ", umax, vmax, wmax)
     msg7 = @sprintf("wall time: %s \n", prettytime(step_time))
 
-    @info msg1 * msg2 * msg4 * msg5 * msg6 * msg7
+    @info msg1
+    # @info msg0 * msg1 * msg2 * msg4 * msg5 * msg6 * msg7
+    # @info msg1 * msg2 * msg4 * msg5 * msg6 * msg7
     # @info msg1 * msg4 * msg5 * msg6 * msg7
 
-     wall_time[] = time_ns()
+    CUDA.memory_status()
+    wall_time[] = time_ns()
+    return nothing
+end
 
-     return nothing
+function gpus_gc(sim)
+    @info "Starting GPU garbage collection for iteration $(iteration(sim))..."
+    @info "before GC:"
+    CUDA.memory_status()
+    wall_time[] = time_ns()
+
+    GC.gc()
+    CUDA.reclaim()
+    @info "after GC:"
+    CUDA.memory_status()
+    step_time = 1e-9 * (time_ns() - wall_time[])
+    @info "GPU garbage collection took $(prettytime(step_time))"
+    wall_time[] = time_ns()
+    return nothing
 end
 
 # And add it as a callback to the simulation.
-add_callback!(omip, progress, IterationInterval(1))
+add_callback!(omip, progress, IterationInterval(10))
+# add_callback!(omip, gpus_gc, IterationInterval(10))
 
 @info "Starting OMIP simulation..."
 run!(omip)
