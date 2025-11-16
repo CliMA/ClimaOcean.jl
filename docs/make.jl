ff9ce702f1352680ea8903dd2823281c06d299a0
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-11-16 17:16:19 +1100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -18,8 +18,8 @@ const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
 to_be_literated = [
-    "single_column_os_papa_simulation.jl",
-    "one_degree_simulation.jl",
+    # "single_column_os_papa_simulation.jl",
+    # "one_degree_simulation.jl",
     "near_global_ocean_simulation.jl"
 ]
 
@@ -44,8 +44,8 @@ pages = [
     "Home" => "index.md",
 
     "Examples" => [
-        "Single-column ocean simulation" => "literated/single_column_os_papa_simulation.md",
-        "One-degree ocean--sea ice simulation" => "literated/one_degree_simulation.md",
+        # "Single-column ocean simulation" => "literated/single_column_os_papa_simulation.md",
+        # "One-degree ocean--sea ice simulation" => "literated/one_degree_simulation.md",
         "Near-global ocean simulation" => "literated/near_global_ocean_simulation.md",
         ],
 
