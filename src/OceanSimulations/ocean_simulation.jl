4ba70a3578cabb1c045991256bd86521576276a5
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-12-01 11:55:01 +1100

    add clock kwarg in docstring

--- src/OceanSimulations/ocean_simulation.jl
+++ src/OceanSimulations/ocean_simulation.jl
@@ -98,6 +98,7 @@ end
 """
     ocean_simulation(grid;
                      Δt = estimate_maximum_Δt(grid),
+                     clock = Clock(grid),
                      closure = default_ocean_closure(),
                      tracers = (:T, :S),
                      free_surface = default_free_surface(grid),
@@ -159,6 +160,7 @@ defaults on a per-field basis.
 ## Keyword Arguments
 
 - `Δt`: Timestep used by the `Simulation`. Defaults to the maximum stable timestep estimated from the `grid`.
+- `clock`: Clock object. Defaults to `Clock(grid)`.
 - `closure`: A turbulence or mixing closure. Defaults to `default_ocean_closure()`.
 - `tracers`: Tuple of tracer names. Defaults to `(:T, :S)`.
 - `free_surface`: Free–surface solver. Defaults to `default_free_surface(grid)`.
