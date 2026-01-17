e972fd01cc80256de89c0b5a1f75eac18f92c501
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-17 10:09:14 -0500

    forgot a where {FT}

--- src/OceanSeaIceModels/InterfaceComputations/sea_ice_ocean_heat_flux_formulations.jl
+++ src/OceanSeaIceModels/InterfaceComputations/sea_ice_ocean_heat_flux_formulations.jl
@@ -195,7 +195,7 @@ const NoInternalFluxTEF{FT} = ThreeEquationHeatFlux{<:Nothing, <:Nothing, FT} wh
 const ConductiveFluxTEF{FT} = ThreeEquationHeatFlux{<:ConductiveFlux, <:AbstractField, FT} where FT
 
 # Helper for internal temperature extraction (used in kernel)
-@inline extract_internal_temperature(::NoInternalFluxTEF{FT}, i, j) = zero(FT)
+@inline extract_internal_temperature(::NoInternalFluxTEF{FT}, i, j) where FT = zero(FT)
 @inline extract_internal_temperature(flux::ConductiveFluxTEF, i, j) = @inbounds flux.internal_temperature[i, j, 1]
 
 """
