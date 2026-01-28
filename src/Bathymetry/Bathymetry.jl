c70bba88976af798930a03bef12912786ce47f53
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-28 16:24:53 +0100

    Framework for computing Ocean basin masks (#722)
    * first commit
    
    * small comment
    
    * remove the extra LatitudeBand
    
    * remove some extra code
    
    * ok these are correct
    
    * fix bathymetry tests
    
    * fix bathymetry tests
    
    * *OceanMask to *_ocean_mask
    
    * fix gpu test
    
    * fix some issues
    
    * make tests pass and add include_southern_ocean kwarg
    
    * correct the whole thing
    
    * all tests pass
    
    * Simplify longitude check in _apply_barrier! function
    
    Refactor longitude check for barrier application logic.
    
    * move on CPU for testing

---
+++ src/Bathymetry/Bathymetry.jl
@@ -0,0 +1,32 @@
+module Bathymetry
+
+export regrid_bathymetry
+export OceanBasinMask
+export atlantic_ocean_mask, indian_ocean_mask, southern_ocean_mask, pacific_ocean_mask
+export label_ocean_basins
+export Barrier
+
+using Downloads
+using ImageMorphology
+using KernelAbstractions: @kernel, @index
+using Oceananigans
+using Oceananigans.Architectures: architecture, on_architecture
+using Oceananigans.BoundaryConditions
+using Oceananigans.DistributedComputations
+using Oceananigans.DistributedComputations: DistributedGrid, reconstruct_global_grid, all_reduce
+using Oceananigans.Fields: interpolate!
+using Oceananigans.Grids: x_domain, y_domain, topology, AbstractGrid
+using Oceananigans.Utils: launch!
+using OffsetArrays
+using NCDatasets
+using Printf
+using Scratch
+
+using ..DataWrangling: Metadatum, native_grid, metadata_path, download_dataset
+using ..DataWrangling.ETOPO: ETOPO2022
+
+include("label_ocean_basins.jl")
+include("regrid_bathymetry.jl")
+include("ocean_basin_mask.jl")
+
+end # module
